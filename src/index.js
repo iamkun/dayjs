9eaaee6564c3f111c479459af83d218a1e1bbff3
iamkun <kunhello@outlook.com>
2018-04-11 16:27:32 +0800

    feat(unix): .unix()
    to unix timestamp

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 class Dayjs {
-  constructor(config = new Date().getTime()) {
-    this.date = new Date(config)
+  constructor(config) {
+    this.utc = false
+    const args = this.parseConfig(config)
+    this.date = new Date(args)
+  }
+
+  parseConfig(config) {
+    if (!config) return new Date()
+    if (/^(\d){8}$/.test(config)) {
+      this.utc = true
+      const y = config.substr(0, 4)
+      const m = config.substr(4, 2)
+      const d = config.substr(6, 2)
+      return `${y}-${m}-${d}`
+    }
+    return false
+  }
+
+  unix() {
+    const zonePad = this.utc ? this.date.getTimezoneOffset() * 60 * 1000 : 0
+    return Math.floor((this.date.getTime() + zonePad) / 1000)
   }
 
   toString() {
-    return this.date.toString().replace(' (CST)', '')
+    return this.date.toUTCString()
   }
 }
 
--- test/index.test.js
+++
@@ -1,6 +0,0 @@
-import moment from 'moment'
-import dayjs from '../src'
-
-test('toString', () => {
-  expect(dayjs().toString()).toBe(moment().toString());
-});
