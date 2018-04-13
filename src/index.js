29db149c6c88deb51970b2c7eeef82dbe76f8bb3
iamkun <kunhello@outlook.com>
2018-04-13 16:48:54 +0800

    fix: fix clone

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { cloneDeep } from 'lodash'
 import * as Constant from './constant'
 
 const padStart = (string, length, pad) => {
@@ -82,6 +83,9 @@ class Dayjs {
         break
       case 'month':
         this.$date.setMonth(int)
+        break
+      case 'year':
+        this.$date.setFullYear(int)
         break
       default:
         break
@@ -179,7 +183,7 @@ class Dayjs {
   }
 
   clone() {
-    return Object.assign(Object.create(this), this)
+    return cloneDeep(this)
   }
 }
 
