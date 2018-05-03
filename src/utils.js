3d06b5aa08c87515fff13a2115679b21011934fb
iamkun <kunhello@outlook.com>
2018-05-03 13:52:02 +0800

    chore(utils comment):

--- src/utils.js
+++ src/utils.js
@@ -7,7 +7,7 @@ export const padStart = (string, length, pad) => {
 export const isNumber = n => (!Number.isNaN(parseFloat(n)) && Number.isFinite(n))
 
 export const monthDiff = (a, b) => {
-  // function from moment.js monthDiff
+  // function from moment.js in order to keep the same result
   const wholeMonthDiff = ((b.year() - a.year()) * 12) + (b.month() - a.month())
   const anchor = a.clone().add(wholeMonthDiff, 'months')
   let anchor2
@@ -19,7 +19,7 @@ export const monthDiff = (a, b) => {
     anchor2 = a.clone().add(wholeMonthDiff + 1, 'months')
     adjust = (b - anchor) / (anchor2 - anchor)
   }
-  return Number(-(wholeMonthDiff + adjust)) || 0
+  return Number(-(wholeMonthDiff + adjust))
 }
 
 export const absFloor = n => (n < 0 ? Math.ceil(n) || 0 : Math.floor(n))
