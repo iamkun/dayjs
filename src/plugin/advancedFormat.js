36964f2a3d4be7c43406ae466d3b93dfb2977ae5
iamkun <kunhello@outlook.com>
2018-05-09 17:34:12 +0800

    refactor: add advancedFormat

--- src/plugin/advancedFormat.js
+++ src/plugin/advancedFormat.js
@@ -1,30 +1,27 @@
-export default (proto) => {
+export default (proto) => { // locale needed
   const oldFormat = proto.format
-  proto.format = function (formatStr = 'YYYY-MM-DDTHH:mm:ssZ') {
-    const oldResult = oldFormat.bind(this)(formatStr)
-
+  proto.format = function (formatStr, locale) {
+    const utils = this.$utils()
+    const str = formatStr || 'YYYY-MM-DDTHH:mm:ssZ'
     const suffixes = ['th', 'st', 'nd', 'rd']
-
-    return oldResult.replace(/Q|ddd|X|x|k{1,2}|S/g, (match) => {
+    const result = str.replace(/Q|Do|X|x|k{1,2}|S/g, (match) => {
       switch (match) {
         case 'Q':
           return Math.ceil((this.$M + 1) / 3)
-        case 'ddd': {
-          const digits = this.$utils().padStart(String(this.$D), 2, '0').slice(-2)
+        case 'Do': {
+          const digits = utils.padStart(String(this.$D), 2, '0').slice(-2)
           const ls = parseInt(digits[1], 10)
-          return this.$D + suffixes[digits[0] === '1' || ls > 3 ? 0 : ls]
+          return `${this.$D}[${suffixes[digits[0] === '1' || ls > 3 ? 0 : ls]}]`
         }
         case 'k':
-          return this.$H === 0 ? 24 : this.$H
         case 'kk':
-          return this.$utils().padStart(String(this.$H === 0 ? 24 : this.$H), 2, '0')
+          return utils.padStart(String(this.$H === 0 ? 24 : this.$H), match === 'k' ? 1 : 2, '0')
         case 'X':
           return Math.floor(this.$d.getTime() / 1000)
-        case 'x':
+        default: // 'x'
           return this.$d.getTime()
-        default: // 'S'
-          return this.$utils().padStart(String(this.$ms), 3, '0')
       }
     })
+    return oldFormat.bind(this)(result, locale)
   }
 }
