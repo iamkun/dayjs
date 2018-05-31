ad31387a2a578a52ce4633468a7548a507973555
Ozgur Erdem <gudubeth@gmail.com>
2018-06-01 01:34:55 +0300

    Improve Turkish localization
    Add relative times to Turkish locale file

--- src/locale/tr.js
+++ src/locale/tr.js
@@ -4,6 +4,21 @@ const locale = {
   name: 'tr',
   weekdays: 'Pazar_Pazartesi_Salı_Çarşamba_Perşembe_Cuma_Cumartesi'.split('_'),
   months: 'Ocak_Şubat_Mart_Nisan_Mayıs_Haziran_Temmuz_Ağustos_Eylül_Ekim_Kasım_Aralık'.split('_'),
+  relativeTime: {
+    future: '%s sonra',
+    past: '%s önce',
+    s: 'birkaç saniye',
+    m: 'bir dakika',
+    mm: '%d dakika',
+    h: 'bir saat',
+    hh: '%d saat',
+    d: 'bir gün',
+    dd: '%d gün',
+    M: 'bir ay',
+    MM: '%d ay',
+    y: 'bir yıl',
+    yy: '%d yıl'
+  },
   ordinal: n => `${n}.`
 }
 
