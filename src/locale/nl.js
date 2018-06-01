bfdae3f9e051736e691ac288421faaf3b499f090
Mahammed Zishan Khan <silent785@hotmail.com>
2018-06-01 09:55:55 +0200

    Add relativeTime support for NL
    Adds relativeTime translations for NL

--- src/locale/nl.js
+++ src/locale/nl.js
@@ -5,6 +5,21 @@ const locale = {
   weekdays: 'Zondag_Maandag_Dinsdag_Woensdag_Donderdag_Vrijdag_Zaterdag'.split('_'),
   months: 'Januari_Februari_Maart_April_Mei_Juni_Juli_Augustus_September_Oktober_November_December'.split('_'),
   ordinal: n => `${n}.`
+  relativeTime = {
+    future: 'over %s',
+    past: '%s geleden',
+    s: 'een paar seconden',
+    m: 'een minuut',
+    mm: '%d minuten',
+    h: 'een uur',
+    hh: '%d uren',
+    d: 'een dag',
+    dd: '%d dagen',
+    M: 'een maand',
+    MM: '%d maanden',
+    y: 'een jaar',
+    yy: '%d jaren'
+  }
 }
 
 dayjs.locale(locale, null, true)
