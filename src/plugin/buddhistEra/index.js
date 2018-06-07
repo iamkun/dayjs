c75c3600ed1ba0f09e77fd9e1d5e6bb41556210f
sak <saknarak@gmail.com>
2018-06-08 00:27:11 +0700

    update to yearBias

--- src/plugin/buddhistEra/index.js
+++ src/plugin/buddhistEra/index.js
@@ -5,14 +5,15 @@ export default (o, c) => { // locale needed later
   const oldFormat = proto.format
   // extend en locale here
   proto.format = function (formatStr, localeObject) {
+    const yearBias = 543
     const locale = localeObject || this.$locale()
     const str = formatStr || FORMAT_DEFAULT
     const result = str.replace(/BBBB|BB/g, (match) => {
       switch (match) {
         case 'BB':
-          return (this.$y + 543) % 100
+          return (this.$y + yearBias) % 100
         default: // BBBB
-          return this.$y + 543
+          return this.$y + yearBias
       }
     })
     return oldFormat.bind(this)(result, locale)
