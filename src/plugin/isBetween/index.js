dfb756b983038e0f612b143707a8eeeca5659fcc
lamartire <lamartire@gmail.com>
2018-08-04 11:09:33 +0300

    Changes logic of isBetween Now it works with non dayjs-like arguments and not depends on order of input

--- src/plugin/isBetween/index.js
+++ src/plugin/isBetween/index.js
@@ -1,11 +1,16 @@
-export default (o, c) => {
+export default (o, c, d) => {
   const proto = c.prototype
   proto.isBetween = function (a, b) {
-    if (!(a instanceof c && b instanceof c)) {
-      throw new Error('isBetween arguments must be dayjs-like!')
+    const dayA = d(a)
+    const dayB = d(b)
+
+    if (this.isAfter(dayA) && this.isBefore(dayB)) {
+      return true
+    } else if (this.isBefore(dayA) && this.isAfter(dayB)) {
+      return true
     }
 
-    return this.isAfter(a) && this.isBefore(b)
+    return false
   }
 }
 
--- test/plugin/isBetween.test.js
+++ test/plugin/isBetween.test.js
@@ -13,15 +13,7 @@ afterEach(() => {
 })
 
 it('is between', () => {
-  expect(dayjs('2018-01-01').isBetween(dayjs('2017-12-31'), dayjs('2018-01-02'))).toBeTruthy()
-  expect(dayjs('2018-01-03 00:01').isBetween(dayjs('2018-01-03 00:00'), dayjs('2018-01-03 00:02'))).toBeTruthy()
-  expect(dayjs('2018-01-03').isBetween(dayjs('2017-12-31'), dayjs('2018-01-02'))).toBeFalsy()
-})
-
-it('if isBetween receive non dayjs-like object – throws error', (done) => {
-  try {
-    dayjs('2018-01-01').isBetween(new Date(), dayjs('2017-12-31'))
-  } catch (err) {
-    done()
-  }
+  expect(dayjs('2018-01-01').isBetween('2017-01-01', '2019-01-01')).toBeTruthy()
+  expect(dayjs('2018-01-01').isBetween('2019-01-01', '2017-01-01')).toBeTruthy()
+  expect(dayjs('2018-01-01').isBetween('2016-01-01', '2017-01-01')).toBeFalsy()
 })
