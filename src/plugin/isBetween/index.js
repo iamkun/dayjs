fc377d81f3af431cb462074ee86c42177cc08cf7
lamartire <lamartire@gmail.com>
2018-08-04 11:32:16 +0300

    Prettify isBetween output

--- src/plugin/isBetween/index.js
+++ src/plugin/isBetween/index.js
@@ -1,16 +1,10 @@
 export default (o, c, d) => {
   const proto = c.prototype
   proto.isBetween = function (a, b) {
-    const dayA = d(a)
-    const dayB = d(b)
+    const dA = d(a)
+    const dB = d(b)
 
-    if (this.isAfter(dayA) && this.isBefore(dayB)) {
-      return true
-    } else if (this.isBefore(dayA) && this.isAfter(dayB)) {
-      return true
-    }
-
-    return false
+    return (this.isAfter(dA) && this.isBefore(dB)) || (this.isBefore(dA) && this.isAfter(dB))
   }
 }
 
