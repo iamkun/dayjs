93e226292db8b7b76b433b196baad2de567352c8
Ferdinand Prantl <prantlf@gmail.com>
2018-08-16 21:07:02 +0200

    Add German localisation for the LocalisableFormat plugin

--- src/locale/de.js
+++ src/locale/de.js
@@ -5,6 +5,14 @@ const locale = {
   weekdays: 'Sonntag_Montag_Dienstag_Mittwoch_Donnerstag_Freitag_Samstag'.split('_'),
   months: 'Januar_Februar_März_April_Mai_Juni_Juli_August_September_Oktober_November_Dezember'.split('_'),
   ordinal: n => `${n}.`,
+  formats: {
+    LTS: 'HH:mm:ss',
+    LT: 'HH:mm',
+    L: 'DD.MM.YYYY',
+    LL: 'D. MMMM YYYY',
+    LLL: 'D. MMMM YYYY HH:mm',
+    LLLL: 'dddd, D. MMMM YYYY HH:mm'
+  },
   relativeTime: {
     future: 'in %s',
     past: 'vor %s',
