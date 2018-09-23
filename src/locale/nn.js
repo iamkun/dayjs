f842eb81f14e26627b75029dafe4f9727737ead0
Simon Dagfinrud <semonje@gmail.com>
2018-09-23 03:07:35 +0200

    Add short names to nn locale

--- src/locale/nn.js
+++ src/locale/nn.js
@@ -3,7 +3,10 @@ import dayjs from 'dayjs'
 const locale = {
   name: 'nn',
   weekdays: 'sundag_måndag_tysdag_onsdag_torsdag_fredag_laurdag'.split('_'),
+  weekdaysShort: 'sun_mån_tys_ons_tor_fre_lau'.split('_'),
+  weekdaysMin: 'su_må_ty_on_to_fr_la'.split('_'),
   months: 'januar_februar_mars_april_mai_juni_juli_august_september_oktober_november_desember'.split('_'),
+  monthsShort: 'jan_feb_mar_apr_mai_jun_jul_aug_sep_okt_nov_des'.split('_'),
   ordinal: n => `${n}.`,
   relativeTime: {
     future: 'om %s',
