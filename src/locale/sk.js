507b36947327a714800ef70b45d90670896f5002
Matúš Duchoň <matus.duchon@gmail.com>
2018-10-10 11:15:42 +0200

    Fix Slovak locale issues (#370)
    * Fix weekdays order
    
    * Month name used in date should be lowercased in slovak
    
    * Remove some leftovers (from spanish?)

--- src/locale/sk.js
+++ src/locale/sk.js
@@ -2,15 +2,15 @@ import dayjs from 'dayjs'
 
 const locale = {
   name: 'sk',
-  weekdays: 'Pondelok_Utorok_Streda_Štvrtok_Piatok_Sobota_Nedeľa'.split('_'),
-  months: 'Január_Február_Marec_Apríl_Máj_Jún_Júl_August_September_Október_November_December'.split('_'),
+  weekdays: 'Nedeľa_Pondelok_Utorok_Streda_Štvrtok_Piatok_Sobota'.split('_'),
+  months: 'január_február_marec_apríl_máj_jún_júl_august_september_október_november_december'.split('_'),
   relativeTime: {
     future: 'o %s',
     past: 'pred %s',
     s: 'niekoľko sekúnd',
     m: 'minúta',
     mm: '%d minút',
-    h: 'un\' hodina',
+    h: 'hodina',
     hh: '%d hodín',
     d: 'deň',
     dd: '%d dní',
