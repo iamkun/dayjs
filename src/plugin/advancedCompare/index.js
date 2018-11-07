a101e3e437724f7498c8f651fd90061c35606389
Horst Schneider <horst.schneider@posteo.de>
2018-11-07 10:40:56 +0100

    Refactor code to reduce bundle size

--- src/plugin/advancedCompare/index.js
+++ src/plugin/advancedCompare/index.js
@@ -1,36 +1,17 @@
-import * as C from '../../constant'
-
 export default (option, dayjsClass) => {
   const proto = dayjsClass.prototype
-  const oldSame = proto.isSame
-  const oldIsAfter = proto.isAfter
-  const oldIsBefore = proto.isBefore
-  const pretty = proto.$utils().prettyUnit
-
   const v = (l, r) => l.isValid() && r.isValid()
 
   proto.isSame = function (o, u) {
-    if (!v(this, o)) return false
-
-    return pretty(u) === C.MS
-      ? oldSame.bind(this)(o)
-      : this.startOf(u).valueOf() <= o && o <= this.endOf(u).valueOf()
+    return v(this, o) && this.startOf(u).valueOf() <= o && o <= this.endOf(u).valueOf()
   }
 
   proto.isAfter = function (o, u) {
-    if (!v(this, o)) return false
-
-    return pretty(u) === C.MS
-      ? oldIsAfter.bind(this)(o)
-      : o.valueOf() < this.startOf(u).valueOf()
+    return v(this, o) && o.valueOf() < this.startOf(u).valueOf()
   }
 
   proto.isBefore = function (o, u) {
-    if (!v(this, o)) return false
-
-    return pretty(u) === C.MS
-      ? oldIsBefore.bind(this)(o)
-      : this.endOf(u).valueOf() < o.valueOf()
+    return v(this, o) && this.endOf(u).valueOf() < o.valueOf()
   }
 
   proto.isSameOrAfter = function (other, units) {
