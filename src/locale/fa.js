235193c8131a2ea6a53d9f88f865ea7e878e5533
Samin Yousefnia <yousefnia@gmail.com>
2018-11-16 17:08:35 +0200

    updating Farsi/Persian locale file

--- src/locale/fa.js
+++ src/locale/fa.js
@@ -1,11 +1,26 @@
-
 import dayjs from 'dayjs'
 
 const locale = {
   name: 'fa',
   weekdays: 'یک‌شنبه_دوشنبه_سه‌شنبه_چهارشنبه_پنج‌شنبه_جمعه_شنبه'.split('_'),
   months: 'ژانویه_فوریه_مارس_آوریل_مه_ژوئن_ژوئیه_اوت_سپتامبر_اکتبر_نوامبر_دسامبر'.split('_'),
-  ordinal: n => n
+  ordinal: n => `${n}`,
+  relativeTime: {
+    // relative time format strings, keep %s %d as the same
+    future: '%s در',
+    past: 'پیش %s',
+    s: 'چند ثانیه',
+    m: 'یک دقیقه',
+    mm: 'دقیقه %d',
+    h: 'یک ساعت',
+    hh: 'ساعت %d',
+    d: 'یک روز',
+    dd: 'روز %d',
+    M: 'یک ماه',
+    MM: 'ماه %d',
+    y: 'یک سال',
+    yy: 'سال %d',
+  },
 }
 
 dayjs.locale(locale, null, true)
