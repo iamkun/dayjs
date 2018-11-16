0a3e7ef956896e2c8e112ac0236bc65d536a4202
Samin Yousefnia <yousefnia@gmail.com>
2018-11-16 17:48:19 +0200

    fixing ordinal function

--- src/locale/fa.js
+++ src/locale/fa.js
@@ -4,7 +4,7 @@ const locale = {
   name: 'fa',
   weekdays: 'یک‌شنبه_دوشنبه_سه‌شنبه_چهارشنبه_پنج‌شنبه_جمعه_شنبه'.split('_'),
   months: 'ژانویه_فوریه_مارس_آوریل_مه_ژوئن_ژوئیه_اوت_سپتامبر_اکتبر_نوامبر_دسامبر'.split('_'),
-  ordinal: n => `${n}`,
+  ordinal: n => n,
   relativeTime: {
     // relative time format strings, keep %s %d as the same
     future: '%s در',
