616bf1a58a78d5837d6220eebb342f698e2ad443
Horst Schneider <horst.schneider@posteo.de>
2018-12-11 11:18:30 +0100

    Add plugin advancedCompare (#404)

--- src/index.js
+++ src/index.js
@@ -92,20 +92,17 @@ class Dayjs {
     return !(this.$d.toString() === 'Invalid Date')
   }
 
-  $compare(that) {
-    return this.valueOf() - dayjs(that).valueOf()
+  isSame(that, units) {
+    const other = dayjs(that)
+    return this.startOf(units) <= other && other <= this.endOf(units)
   }
 
-  isSame(that) {
-    return this.$compare(that) === 0
+  isAfter(that, units) {
+    return dayjs(that) < this.startOf(units)
   }
 
-  isBefore(that) {
-    return this.$compare(that) < 0
-  }
-
-  isAfter(that) {
-    return this.$compare(that) > 0
+  isBefore(that, units) {
+    return this.endOf(units) < dayjs(that)
   }
 
   year() {
