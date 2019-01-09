6cd01fe567611e83db863cf726bef95353f8784e
Nurbek <nurbek-2395@mail.ru>
2019-01-09 07:35:26 +0500

    chore: added locale kz (Kazakh) (#443)
    * added locale kz
    
    * Fix: extra whitespaces
    
    * Changed kz to kk

---
+++ src/locale/kk.js
@@ -0,0 +1,30 @@
+import dayjs from 'dayjs'
+
+const locale = {
+  name: 'kk',
+  weekdays: 'жексенбі_дүйсенбі_сейсенбі_сәрсенбі_бейсенбі_жұма_сенбі'.split('_'),
+  weekdaysShort: 'жек_дүй_сей_сәр_бей_жұм_сен'.split('_'),
+  weekdaysMin: 'жк_дй_сй_ср_бй_жм_сн'.split('_'),
+  months: 'қаңтар_ақпан_наурыз_сәуір_мамыр_маусым_шілде_тамыз_қыркүйек_қазан_қараша_желтоқсан'.split('_'),
+  monthsShort: 'қаң_ақп_нау_сәу_мам_мау_шіл_там_қыр_қаз_қар_жел'.split('_'),
+  relativeTime: {
+    future: '%s ішінде',
+    past: '%s бұрын',
+    s: 'бірнеше секунд',
+    m: 'бір минут',
+    mm: '%d минут',
+    h: 'бір сағат',
+    hh: '%d сағат',
+    d: 'бір күн',
+    dd: '%d күн',
+    M: 'бір ай',
+    MM: '%d ай',
+    y: 'бір жыл',
+    yy: '%d жыл'
+  },
+  ordinal: n => n
+}
+
+dayjs.locale(locale, null, true)
+
+export default locale
