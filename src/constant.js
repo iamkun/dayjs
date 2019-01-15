c9ca021594b6bbe2f2e030d9ec4444abad4f08de
Arnau Lacambra <naulacambra@gmail.com>
2019-01-15 17:48:50 +0100

    Added deltaZone in the equation when calculatin diff

--- src/constant.js
+++ src/constant.js
@@ -34,4 +34,3 @@ export const en = {
   weekdays: 'Sunday_Monday_Tuesday_Wednesday_Thursday_Friday_Saturday'.split('_'),
   months: 'January_February_March_April_May_June_July_August_September_October_November_December'.split('_')
 }
-
