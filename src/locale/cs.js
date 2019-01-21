62c908238e9f647ba848eda79bb42b915b03bfd6
Dennis Fridrich <fridrich.dennis@gmail.com>
2019-01-21 14:57:45 +0100

    Fix CS

--- src/locale/cs.js
+++ src/locale/cs.js
@@ -1,25 +1,25 @@
 import dayjs from 'dayjs'
 
 const locale = {
-	name: 'cs',
-	weekdays: 'Neděle_Pondělí_Úterý_Středa_Čtvrtek_Pátek_Sobota'.split('_'),
-	months: 'leden_únor_březen_duben_květen_červen_červenec_srpen_září_říjen_listopad_prosinec'.split('_'),
-	relativeTime: {
-		future: 'za %s',
-		past: 'před %s',
-		s: 'několik sekund',
-		m: 'minuta',
-		mm: '%d minut',
-		h: 'hodina',
-		hh: '%d hodin',
-		d: 'den',
-		dd: '%d dnů',
-		M: 'měsíc',
-		MM: '%d měsíců',
-		y: 'rok',
-		yy: '%d roků'
-	},
-	ordinal: n => `${n}º`
+  name: 'cs',
+  weekdays: 'Neděle_Pondělí_Úterý_Středa_Čtvrtek_Pátek_Sobota'.split('_'),
+  months: 'leden_únor_březen_duben_květen_červen_červenec_srpen_září_říjen_listopad_prosinec'.split('_'),
+  relativeTime: {
+    future: 'za %s',
+    past: 'před %s',
+    s: 'několik sekund',
+    m: 'minuta',
+    mm: '%d minut',
+    h: 'hodina',
+    hh: '%d hodin',
+    d: 'den',
+    dd: '%d dnů',
+    M: 'měsíc',
+    MM: '%d měsíců',
+    y: 'rok',
+    yy: '%d roků'
+  },
+  ordinal: n => `${n}º`
 }
 
 dayjs.locale(locale, null, true)
