a88f2f85c4035e859168a8f99a86f44b272345ff
Yizhe Wang <wangyiz4262+git@gmail.com>
2019-01-27 11:31:22 -0500

    Fix typings to correctly reflect the usage

--- index.d.ts
+++ index.d.ts
@@ -1,97 +1,77 @@
-export = dayjs;
+export default dayjs
 
-declare function dayjs (config?: dayjs.ConfigType, option?: dayjs.OptionType): dayjs.Dayjs
+declare const dayjs: DayjsFn
 
-declare namespace dayjs {
-  export type ConfigType = string | number | Date | Dayjs
+type DayjsFn = {
+  (config?: ConfigType, option?: OptionType): DayjsInstance
+  extend(plugin: PluginFunc, option?: ConfigType): DayjsInstance
+  locale(arg1: any, arg2?: any): string
+  isDayjs(d: any): d is DayjsInstance
+  unix(t: number): DayjsInstance
+}
 
-  export type OptionType = { locale: string }
+export type ConfigType = string | number | Date | DayjsInstance
 
-  export type UnitType = 'millisecond' | 'second' | 'minute' | 'hour' | 'day' | 'week' | 'month' | 'quarter' | 'year' | 'date'
+export type OptionType = { locale: string }
 
-  interface DayjsObject {
-    years: number
-    months: number
-    date: number
-    hours: number
-    minutes: number
-    seconds: number
-    milliseconds: number
-  }
+export type UnitType =
+  | 'millisecond'
+  | 'second'
+  | 'minute'
+  | 'hour'
+  | 'day'
+  | 'week'
+  | 'month'
+  | 'quarter'
+  | 'year'
+  | 'date'
 
-  class Dayjs {
-    constructor (config?: ConfigType)
+export type PluginFunc = (
+  option: ConfigType,
+  d1: DayjsInstance,
+  d2: DayjsInstance
+) => void
 
-    clone(): Dayjs
+export type DayjsPlainObject = {
+  years: number
+  months: number
+  date: number
+  hours: number
+  minutes: number
+  seconds: number
+  milliseconds: number
+}
 
-    isValid(): boolean
-
-    year(): number
-
-    month(): number
-
-    date(): number
-
-    day(): number
-
-    hour(): number
-
-    minute(): number
-
-    second(): number
-
-    millisecond(): number
-
-    set(unit: UnitType, value: number): Dayjs
-
-    add(value: number, unit: UnitType): Dayjs
-
-    subtract(value: number, unit: UnitType): Dayjs
-
-    startOf(unit: UnitType): Dayjs
-
-    endOf(unit: UnitType): Dayjs
-
-    format(template?: string): string
-
-    diff(dayjs: Dayjs, unit: UnitType, float?: boolean): number
-
-    valueOf(): number
-
-    unix(): number
-
-    daysInMonth(): number
-
-    toDate(): Date
-
-    toArray(): number[]
-
-    toJSON(): string
-
-    toISOString(): string
-
-    toObject(): DayjsObject
-
-    toString(): string
-
-    isBefore(dayjs: Dayjs, unit?: UnitType): boolean
-
-    isSame(dayjs: Dayjs, unit?: UnitType): boolean
-
-    isAfter(dayjs: Dayjs, unit?: UnitType): boolean
-
-    isLeapYear(): boolean
-
-    locale(arg1: any, arg2?: any): Dayjs
-  }
-
-  export type PluginFunc = (option: ConfigType, d1: Dayjs, d2: Dayjs) => void
-
-  export function extend(plugin: PluginFunc, option?: ConfigType): Dayjs
-
-  export function locale(arg1: any, arg2?: any): string
-
-  export function isDayjs(d: any): d is Dayjs
-  
-  export function unix(t: number): Dayjs
+export type DayjsInstance = {
+  clone(): DayjsInstance
+  isValid(): boolean
+  year(): number
+  month(): number
+  date(): number
+  day(): number
+  hour(): number
+  minute(): number
+  second(): number
+  millisecond(): number
+  set(unit: UnitType, value: number): DayjsInstance
+  add(value: number, unit: UnitType): DayjsInstance
+  subtract(value: number, unit: UnitType): DayjsInstance
+  startOf(unit: UnitType): DayjsInstance
+  endOf(unit: UnitType): DayjsInstance
+  format(template?: string): string
+  diff(dayjs: DayjsInstance, unit: UnitType, float?: boolean): number
+  valueOf(): number
+  unix(): number
+  daysInMonth(): number
+  toDate(): Date
+  toArray(): number[]
+  toJSON(): string
+  toISOString(): string
+  toObject(): DayjsPlainObject
+  toString(): string
+  isBefore(dayjs: DayjsInstance, unit?: UnitType): boolean
+  isSame(dayjs: DayjsInstance, unit?: UnitType): boolean
+  isAfter(dayjs: DayjsInstance, unit?: UnitType): boolean
+  isLeapYear(): boolean
+  locale(arg1: any, arg2?: any): DayjsInstance
 }
