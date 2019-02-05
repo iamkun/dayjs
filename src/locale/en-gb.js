a71a52db7b65581685129a58381ba250fb9677fa
Tom <tcrands@gmail.com>
2019-02-05 14:53:46 +0000

    Added en-gb-locale

---
+++ src/locale/en-gb.js
@@ -0,0 +1,49 @@
+import dayjs from 'dayjs'
+
+const locale = {
+  name: 'en',
+  weekdays: 'Sunday_Monday_Tuesday_Wednesday_Thursday_Friday_Saturday'.split('_'),
+  weekdaysShort: 'Sun_Mon_Tue_Wed_Thu_Fri_Sat'.split('_'),
+  weekdaysMin: 'Su_Mo_Tu_We_Th_Fr_Sa'.split('_'),
+  months: 'January_February_March_April_May_June_July_August_September_October_November_December'.split('_'),
+  monthsShort: 'Jan_Feb_Mar_Apr_May_Jun_Jul_Aug_Sep_Oct_Nov_Dec'.split('_'),
+  relativeTime: {
+    future: 'in %s',
+    past: '%s ago',
+    s: 'a few seconds',
+    m: 'a minute',
+    mm: '%d minutes',
+    h: 'an hour',
+    hh: '%d hours',
+    d: 'a day',
+    dd: '%d days',
+    M: 'a month',
+    MM: '%d months',
+    y: 'a year',
+    yy: '%d years'
+  },
+  formats: {
+    LT: 'HH:mm',
+    LTS: 'HH:mm:ss',
+    L: 'DD/MM/YYYY',
+    LL: 'D MMMM YYYY',
+    LLL: 'D MMMM YYYY HH:mm',
+    LLLL: 'dddd, D MMMM YYYY HH:mm'
+  },
+  ordinal(number) {
+    const b = number % 10
+    let output = 'th'
+    if (b === 1) {
+      output = 'st'
+    } else if (b === 2) {
+      output = 'nd'
+    } else if (b === 3) {
+      output = 'rd'
+    }
+    return number + output
+  }
+}
+
+dayjs.locale(locale, null, true)
+
+export default locale
