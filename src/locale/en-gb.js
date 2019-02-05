ce078013c41abc1da0cd3dabd5fae4c52856e4bd
Tom Rands <tcrands@gmail.com>
2019-02-05 15:07:01 +0000

    Added week start config

--- src/locale/en-gb.js
+++ src/locale/en-gb.js
@@ -7,6 +7,7 @@ const locale = {
   weekdaysMin: 'Su_Mo_Tu_We_Th_Fr_Sa'.split('_'),
   months: 'January_February_March_April_May_June_July_August_September_October_November_December'.split('_'),
   monthsShort: 'Jan_Feb_Mar_Apr_May_Jun_Jul_Aug_Sep_Oct_Nov_Dec'.split('_'),
+  weekStart: 1,
   relativeTime: {
     future: 'in %s',
     past: '%s ago',
