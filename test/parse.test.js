d4cd3adda076210e72370cc148eb5ea0c127d278
Horst Schneider <horst.schneider@posteo.de>
2019-02-18 12:24:36 +0100

    chore: Refactor parseDate (#487)

--- test/parse.test.js
+++ test/parse.test.js
@@ -15,7 +15,7 @@ describe('Parse', () => {
     expect(dayjs().valueOf()).toBe(moment().valueOf())
   })
 
-  it('String 20130208', () => {
+  it('moment-js like formatted dates', () => {
     global.console.warn = jest.genMockFunction()// moment.js '2018-4-1 1:1:1:22' will throw warn
     let d = '20130108'
     expect(dayjs(d).valueOf()).toBe(moment(d).valueOf())
@@ -44,6 +44,40 @@ describe('Parse', () => {
   it('String ISO 8601 date, time and zone', () => {
     const time = '2018-04-04T16:00:00.000Z'
     expect(dayjs(time).valueOf()).toBe(moment(time).valueOf())
+  })
+
+  it('String RFC 2822, time and zone', () => {
+    const time = 'Mon, 11 Feb 2019 09:46:50 GMT+1'
+    const expected = '2019-02-11T08:46:50.000Z'
+    const d = dayjs(time)
+    expect(d.toISOString()).toEqual(expected)
+    expect(d.valueOf()).toBe(moment(time).valueOf())
+  })
+
+  it('String ECMAScript, time and zone', () => {
+    // should parse dates formatted in ECMA script format
+    // see https://www.ecma-international.org/ecma-262/9.0/index.html#sec-date.prototype.tostring
+    const time = 'Mon Feb 11 2019 11:01:37 GMT+0100 (Mitteleuropäische Normalzeit)'
+    const expected = '2019-02-11T10:01:37.000Z'
+    const d = dayjs(time)
+    expect(d.toISOString()).toEqual(expected)
+    expect(d.valueOf()).toBe(moment(time).valueOf())
+  })
+
+  it('rejects invalid values', () => {
+    expect(dayjs({}).isValid()).toBe(false)
+    expect(dayjs(() => '2018-01-01').isValid()).toBe(false)
+    expect(dayjs(Infinity).isValid()).toBe(false)
+    expect(dayjs(NaN).isValid()).toBe(false)
+    expect(dayjs([2018, 5, 1, 13, 52, 44]).isValid()).toBe(false) // Arrays with time part
+  })
+
+  it('parses Arrays with date part', () => {
+    const dateParts = [2018, 5, 1]
+    const expected = '2018-05-01T00:00:00.000Z'
+    const d = dayjs(dateParts)
+    const normalized = d.add(d.utcOffset(), 'minutes') // make test run in every timezone
+    expect(normalized.toISOString()).toEqual(expected)
   })
 
   it('String Other, Null and isValid', () => {
