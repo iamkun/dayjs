44e9680e66ba76c638e953099a4d1726a0a56f16
iamkun <kunhello@outlook.com>
2019-02-24 00:55:44 +0800

    chore: update isBetween.d.ts

--- types/plugin/isBetween.d.ts
+++ types/plugin/isBetween.d.ts
@@ -1,10 +1,10 @@
-import { PluginFunc, ConfigType } from 'dayjs'
+import { PluginFunc, ConfigType, OpUnitType } from 'dayjs'
 
 declare const plugin: PluginFunc
 export = plugin
 
 declare module 'dayjs' {
   interface Dayjs {
-    isBetween(a: ConfigType, b: ConfigType): boolean
+    isBetween(a: ConfigType, b: ConfigType, c?: OpUnitType | null, d?: string): boolean
   }
 }
