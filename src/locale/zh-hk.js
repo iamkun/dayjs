cc9386ac279a9c955071db1647ec248dca921558
iamkun <kunhello@outlook.com>
2019-03-05 18:40:50 +0800

    Update zh-hk.js

--- src/locale/zh-hk.js
+++ src/locale/zh-hk.js
@@ -2,11 +2,11 @@ import dayjs from 'dayjs'
 
 const locale = {
   name: 'zh-hk',
-  weekdays: '星期日_星期一_星期二_星期三_星期四_星期五_星期六'.split('_'),
-  weekdaysShort: '星期日_星期一_星期二_星期三_星期四_星期五_星期六'.split('_'),
-  weekdaysMin: '星期日_星期一_星期二_星期三_星期四_星期五_星期六'.split('_'),
-  months: '一月_二月_三月_四月_五月_六月_七月_八月_九月_十月_十一月_十二月'.split('_'),
-  monthsShort: '一月_二月_三月_四月_五月_六月_七月_八月_九月_十月_十一月_十二月'.split('_'),
+  months : '一月_二月_三月_四月_五月_六月_七月_八月_九月_十月_十一月_十二月'.split('_'),
+  monthsShort : '1月_2月_3月_4月_5月_6月_7月_8月_9月_10月_11月_12月'.split('_'),
+  weekdays : '星期日_星期一_星期二_星期三_星期四_星期五_星期六'.split('_'),
+  weekdaysShort : '週日_週一_週二_週三_週四_週五_週六'.split('_'),
+  weekdaysMin : '日_一_二_三_四_五_六'.split('_'),
   ordinal: n => `${n}日`,
   formats: {
     LT: 'HH:mm',
@@ -17,9 +17,9 @@ const locale = {
     LLLL: 'YYYY年M月D日dddd HH:mm'
   },
   relativeTime: {
-    future: '於 %s',
-    past: '%s 前',
-    s: '數秒',
+    future : '%s內',
+    past : '%s前',
+    s : '幾秒',
     m: '一分鐘',
     mm: '%d 分鐘',
     h: '一小時',
@@ -27,7 +27,7 @@ const locale = {
     d: '一天',
     dd: '%d 天',
     M: '一個月',
-    MM: '%d 月',
+    MM: '%d 個月',
     y: '一年',
     yy: '%d 年'
   }
