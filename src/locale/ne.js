bdbec0127e969477f35da2351aea0a5a9bf4d4bf
Jitendra Adhikari <jiten.adhikary@gmail.com>
2019-03-09 23:11:38 +0700

    fix(locale): Add nepali (ne) locale (#524)

---
+++ src/locale/ne.js
@@ -0,0 +1,27 @@
+import dayjs from 'dayjs'
+
+const locale = {
+  name: 'ne',
+  weekdays: 'आइतबार_सोमबार_मङ्गलबार_बुधबार_बिहिबार_शुक्रबार_शनिबार'.split('_'),
+  months: 'जनवरी_फेब्रुवरी_मार्च_अप्रिल_मे_जुन_जुलाई_अगष्ट_सेप्टेम्बर_अक्टोबर_नोभेम्बर_डिसेम्बर'.split('_'),
+  relativeTime: {
+    future: '%s पछि',
+    past: '%s अघि',
+    s: 'सेकेन्ड',
+    m: 'एक मिनेट',
+    mm: '%d मिनेट',
+    h: 'घन्टा',
+    hh: '%d घन्टा',
+    d: 'एक दिन',
+    dd: '%d दिन',
+    M: 'एक महिना',
+    MM: '%d महिना',
+    y: 'एक वर्ष',
+    yy: '%d वर्ष'
+  },
+  ordinal: n => `${n}`.replace(/\d/g, i => '०१२३४५६७८९'[i])
+}
+
+dayjs.locale(locale, null, true)
+
+export default locale
