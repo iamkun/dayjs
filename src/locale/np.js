4c0a8060af398e12d4f61f98aed432660fa0298c
Jitendra Adhikari <jiten.adhikary@gmail.com>
2019-03-09 12:34:18 +0700

    feat(locale): add nepali (np) locale

---
+++ src/locale/np.js
@@ -0,0 +1,27 @@
+import dayjs from 'dayjs'
+
+const locale = {
+  name: 'np',
+  weekdays: 'आइतबार_सोमबार_मङ्गलबार_बुधबार_बिहिबार_शुक्रबार_शनिबार'.split('_'),
+  months: 'वैशाख_जेठ_आषाढ_श्रावण_भाद्र_असोज_कार्तिक_मङ्सिर_पौष_माघ_फाल्गुन_चैत्र '.split('_'),
+  relativeTime: {
+    future: '%s पछि',
+    past: '%s अघि',
+    s: 'सेकेन्ड',
+    m: 'मिनेट',
+    mm: '%d मिनेट',
+    h: 'घन्टा',
+    hh: '%d घन्टा',
+    d: 'दिन',
+    dd: '%d दिन',
+    M: 'महिना',
+    MM: '%d महिना',
+    y: 'वर्ष',
+    yy: '%d वर्ष'
+  },
+  ordinal: n => (n + '').replace(/\d/g, i => '०१२३४५६७८९'[i])
+}
+
+dayjs.locale(locale, null, true)
+
+export default locale
