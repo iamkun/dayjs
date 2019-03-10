a8926082ee11979866e1328214443ae1823f5f07
iamkun <kunhello@outlook.com>
2019-03-10 18:32:14 +0800

    fix: Add WeekYear plugin

--- src/locale/zh-cn.js
+++ src/locale/zh-cn.js
@@ -7,7 +7,6 @@ const locale = {
   weekdaysMin: '日_一_二_三_四_五_六'.split('_'),
   months: '一月_二月_三月_四月_五月_六月_七月_八月_九月_十月_十一月_十二月'.split('_'),
   monthsShort: '1月_2月_3月_4月_5月_6月_7月_8月_9月_10月_11月_12月'.split('_'),
-  ordinal1: n => `${n}日`,
   ordinal: (number, period) => {
     switch (period) {
       case 'W':
