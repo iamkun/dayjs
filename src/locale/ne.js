877ca61cf362e4469043b78a0a020cfe356f2a3b
Jitendra Adhikari <jiten.adhikary@gmail.com>
2019-03-10 14:29:41 +0700

    feat(locale.ne): add weekdays short + min

--- src/locale/ne.js
+++ src/locale/ne.js
@@ -3,6 +3,8 @@ import dayjs from 'dayjs'
 const locale = {
   name: 'ne',
   weekdays: 'आइतबार_सोमबार_मङ्गलबार_बुधबार_बिहिबार_शुक्रबार_शनिबार'.split('_'),
+  weekdaysShort: 'आइत_सोम_मङ्गल_बुध_बिहि_शुक्_शनि'.split('_'),
+  weekdaysMin: 'आ_सो_म_बु_बि_शु_श'.split('_'),
   months: 'जनवरी_फेब्रुवरी_मार्च_अप्रिल_मे_जुन_जुलाई_अगष्ट_सेप्टेम्बर_अक्टोबर_नोभेम्बर_डिसेम्बर'.split('_'),
   relativeTime: {
     future: '%s पछि',
