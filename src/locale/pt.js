0b87dd819aa0f2e52c53d08f740cf9eff5ed9a92
ptdev <gomes.roberto@gmail.com>
2019-03-18 15:34:46 +0000

    Improved PT locale
    Added some missing properties and localized formats

--- src/locale/pt.js
+++ src/locale/pt.js
@@ -3,9 +3,19 @@ import dayjs from 'dayjs'
 const locale = {
   name: 'pt',
   weekdays: 'Domingo_Segunda-feira_Terça-feira_Quarta-feira_Quinta-feira_Sexta-feira_Sábado'.split('_'),
+  weekdaysShort: 'Dom_Seg_Ter_Qua_Qui_Sex_Sab'.split('_'),
   months: 'Janeiro_Fevereiro_Março_Abril_Maio_Junho_Julho_Agosto_Setembro_Outubro_Novembro_Dezembro'.split('_'),
+  monthsShort: 'Jan_Fev_Mar_Abr_Mai_Jun_Jul_Ago_Set_Out_Nov_Dez'.split('_'),
   ordinal: n => `${n}º`,
   weekStart: 1,
+  formats: {
+    LT: 'HH:mm',
+    LTS: 'HH:mm:ss',
+    L: 'DD/MM/YYYY',
+    LL: 'D [de] MMMM [de] YYYY',
+    LLL: 'D [de] MMMM [de] YYYY [às] HH:mm',
+    LLLL: 'dddd, D [de] MMMM [de] YYYY [às] HH:mm'
+  },
   relativeTime: {
     future: 'em %s',
     past: 'há %s',
