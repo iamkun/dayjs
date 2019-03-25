317fd3e6893c3f1cd60165c207bf88d4fd2e33bf
HoSung Lim <holnet1026@gmail.com>
2019-03-25 12:13:58 +0900

    fix: Update ko locale weekdaysShort  (#543)

--- src/locale/ko.js
+++ src/locale/ko.js
@@ -3,6 +3,7 @@ import dayjs from 'dayjs'
 const locale = {
   name: 'ko',
   weekdays: '일요일_월요일_화요일_수요일_목요일_금요일_토요일'.split('_'),
+  weekdaysShort: '일_월_화_수_목_금_토'.split('_'),
   months: '1월_2월_3월_4월_5월_6월_7월_8월_9월_10월_11월_12월'.split('_'),
   ordinal: n => n,
   formats: {
