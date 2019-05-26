56e720285de12e22986f6ec22ac281c5a5f231d6
Ronny Hanssen <super.ronny@gmail.com>
2019-05-27 00:27:37 +0200

    Added short forms for weeks and months for Norsk Bokmål

--- src/locale/nb.js
+++ src/locale/nb.js
@@ -3,7 +3,10 @@ import dayjs from 'dayjs'
 const locale = {
   name: 'nb',
   weekdays: 'søndag_mandag_tirsdag_onsdag_torsdag_fredag_lørdag'.split('_'),
+  weekdaysShort: 'søn_man_tir_ons_tor_fre_lør'.split('_'),
+  weekdaysMin: 'sø_ma_ti_on_to_fr_lø'.split('_'),  
   months: 'januar_februar_mars_april_mai_juni_juli_august_september_oktober_november_desember'.split('_'),
+  monthsShort: 'jan_feb_mar_apr_mai_jun_jul_aug_sep_okt_nov_des'.split('_'),
   ordinal: n => `${n}.`,
   weekStart: 1,
   formats: {
@@ -34,4 +37,3 @@ const locale = {
 dayjs.locale(locale, null, true)
 
 export default locale
-
