fbd157a2d2c76356eebd8cb614a0900d226dd75c
janat08 <jey.and.key@gmail.com>
2019-06-03 09:46:47 +0600

    remove dependency

--- src/plugin/timezones/index.js
+++ src/plugin/timezones/index.js
@@ -1,60 +1,23 @@
-import { utcToZonedTime, toDate } from 'date-fns-tz'
 import polyfill from './polyfillLoader'
-import dayjs from '../..'
-// basically smaller copy from date-fns-tz
-function zonedTimeToUtc(date, timeZone) {
-  return toDate(date, { timeZone })
-}
-// considering just calculating offset
+
 export default (o, c, d) => {
   polyfill()
   const proto = c.prototype
-  // zonedTimeToUtc expects formated input, therefore with the custom version a dayjs instance
-  // proto.zonedTimeToUtc = zonedTimeToUtc
-  // proto.utcToZonedTime = utcToZonedTime
-  proto.toUTC = function (tz = Intl.DateTimeFormat().resolvedOptions().timeZone) {
-    const utils = this.$utils()
-    return utils.w(zonedTimeToUtc(
-      this.format('YYYY-MM-DDTHH:mm:ss'),
-      tz
-    ), this)
-  }
-  proto.toZone = function (tz = Intl.DateTimeFormat().resolvedOptions().timeZone) {
-    const utils = this.$utils()
-    return utils.w(utcToZonedTime(
-      this.$d,
-      tz
-    ), this)
-  }
   // constructor
   d.tz = function (date, tz) {
-    const zoned = new Date(dayjs(date).toDate().toLocaleString('en-US', { timeZone: tz }))
+    const zoned = new Date(d(date).toDate().toLocaleString('en-US', { timeZone: tz }))
     // for getTimezoneOffset in minutes used for output thats rounded anyway
-    const difference = (dayjs(date).toDate() - zoned) / 1000 / 60
-    // new Date(date).toLocaleString("en-US", { timeZone: tz })
-
-    return dayjs(date, { tzOffsetModifier: difference, timeZone: tz })
+    const difference = (d(date).toDate() - zoned) / 1000 / 60
+    return d(date, { tzOffsetModifier: difference, timeZone: tz })
   }
   proto.tz = function (tz) {
     const date = new Date(Date.parse(this.toDate()) + (this.$tzOffsetModifier * 1000 * 60))
-    const utils = this.$utils()
     const zoned = new Date(date.toLocaleString('en-US', { timeZone: tz }))
     // for getTimezoneOffset in minutes used for output thats rounded anyway
     const difference = (date - zoned) / 1000 / 60
-    // new Date(date).toLocaleString("en-US", { timeZone: tz })
-    this.$tzOffsetModifier = difference
-    this.timeZone = tz
-    return utils.w(zoned, this)
-  }
-  proto.tz = function (tz) {
-    const date = new Date(Date.parse(this.toDate()) + (this.$tzOffsetModifier * 1000 * 60))
-    const utils = this.$utils()
-    const zoned = new Date(date.toLocaleString('en-US', { timeZone: tz }))
-    // for getTimezoneOffset in minutes used for output thats rounded anyway
-    const difference = (date - zoned) / 1000 / 60
-    // new Date(date).toLocaleString("en-US", { timeZone: tz })
-    this.$tzOffsetModifier = difference
-    this.timeZone = tz
-    return utils.w(zoned, this)
+    const clone = d(zoned)
+    clone.$tzOffsetModifier = difference
+    clone.timeZone = tz
+    return clone
   }
 }
--- test/plugin/timezones.test.js
+++ test/plugin/timezones.test.js
@@ -1,8 +1,5 @@
 import MockDate from 'mockdate'
-import {
-  zonedTimeToUtc,
-  utcToZonedTime
-} from 'date-fns-tz'
+import { utcToZonedTime } from 'date-fns-tz'
 import dayjs from '../../src'
 import tz from '../../src/plugin/timezones'
 
@@ -15,27 +12,11 @@ afterEach(() => {
   MockDate.reset()
 })
 describe('timezone plugin', () => {
-  it('converts to UTC', () => {
-    const date = dayjs(new Date(1559387786000)) // 2019-06-01T17:16:26+06:00
-    expect(date.toUTC('America/New_York').toDate()).toEqual(zonedTimeToUtc(new Date(1559387786000), 'America/New_York'))
-    expect(date.toUTC('Asia/Almaty').toDate()).toEqual(zonedTimeToUtc(new Date(1559387786000), 'Asia/Almaty'))
-  })
   it('converts from UTC', () => {
     const or = new Date(Date.UTC(96, 1, 2, 3, 4, 5))
     const date = dayjs(or) // 2019-06-01T17:16:26+06:00
-    expect(date.toZone('America/New_York').toDate()).toEqual(utcToZonedTime(or, 'America/New_York'))
-    expect(date.toZone('Asia/Almaty').toDate()).toEqual(utcToZonedTime(or, 'Asia/Almaty'))
-  })
-  it('parses offset', () => {
-    function date(st) {
-      return new Date(st).toLocaleString('en-US', { timeZone: 'America/New_York' })
-    }
-    const b = date('2013-06-01T05:00:00-04:00')
-    const c = date('2013-06-01T05:00:00+00:00')
-    console.log(
-      b.toLocaleString('en-US', { timeZone: 'America/New_York', timeZoneName: 'long' }),
-      new Date(c).toLocaleString('en-US', { timeZone: 'America/New_York', timeZoneName: 'long' })
-    )
+    expect(date.tz('America/New_York').toDate()).toEqual(utcToZonedTime(or, 'America/New_York'))
+    expect(date.tz('Asia/Almaty').toDate()).toEqual(utcToZonedTime(or, 'Asia/Almaty'))
   })
   it('takes constructor on tz', () => {
     const a = dayjs.tz('2013-11-18 11:55', 'Asia/Taipei')
@@ -47,25 +28,12 @@ describe('timezone plugin', () => {
     expect(a.utc().format()).toEqual('2013-11-18T03:55:00+00:00') // 2013-11-18T03:55Z
     expect(b.utc().format()).toEqual('2013-11-18T16:55:00+00:00') // 2013-11-18T16:55Z
   })
-  it.only('converts to tz', () => {
-    const a = dayjs('2013-11-18 11:55').tz('Asia/Taipei')
-    const b = dayjs('2013-11-18 11:55').tz('America/Toronto')
+  it('converts to tz', () => {
+    // no daylight savings
+    const a = dayjs.tz('2013-11-18 11:55', 'Asia/Almaty').tz('Etc/utc')
+    const b = dayjs.tz('2013-11-18 11:55', 'Etc/utc').tz('Asia/Almaty')
 
-    expect(a.format()).toEqual('2013-11-18T19:55:00+08:00') // 2013-11-18T19:55:00+08:00
-    expect(b.format()).toEqual('2013-11-18T06:55:00-05:00') // 2013-11-18T06:55:00-05:00
-
-    expect(a.utc().format()).toEqual('2013-11-18T11:55Z') // 2013-11-18T11:55Z
-    expect(b.utc().format()).toEqual('2013-11-18T11:55Z') // 2013-11-18T11:55Z
-  })
-  it('offset', () => {
-    function date(st) {
-      return new Date(st).toLocaleString('en-US', { timeZone: 'America/New_York' })
-    }
-    const b = date('2013-06-01T05:00:00-04:00')
-    const c = date('2013-06-01T05:00:00+00:00')
-    console.log(
-      new Date(b).getTimezoneOffset(),
-      new Date(c).getTimezoneOffset()
-    )
+    expect(a.format()).toEqual('2013-11-18T05:55:00+00:00') // 2013-11-18T19:55:00+08:00
+    expect(b.format()).toEqual('2013-11-18T17:55:00+06:00') // 2013-11-18T06:55:00-05:00
   })
 })
