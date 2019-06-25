268bd919980a79a135aae9be21a6a57396a099ac
mehdi <lockee14@msn.com>
2019-06-26 00:42:37 +0900

    refactor: shorter version

--- src/plugin/duration/index.js
+++ src/plugin/duration/index.js
@@ -11,101 +11,14 @@ export default (o, c) => {
       return {}
     }
 
-    // P[n]Y[n]M[n]DT[n]H[n]M[n]S
-    // other format P[YYYY]-[MM]-[DD]T[hh]:[mm]:[ss]
-    /**
-    * convert a number to his time unit
-    * @param  {number} v a number
-    * @param  {string} u unit of time of the number
-    * @return {array[r, nextUnit]} return next time unit and is value to parse
-    */
-    function parse(v, u, duration) {
-      let r = 0
-      let nextUnit = ''
-      switch (u) {
-        case 'seconde':
-          r = Math.floor(v / 60)
-          if (r >= 1) {
-            duration.s = v % 60
-            duration.ISO = `${duration.s}S${duration.ISO}`
-            nextUnit = 'minute'
-          } else {
-            duration.s = v
-            duration.ISO = `${duration.s}S${duration.ISO}`
-            nextUnit = ''
-          }
-          break
-        case 'minute':
-          r = Math.floor(v / 60)
-          if (r >= 1) {
-            duration.m = v % 60
-            duration.ISO = `${duration.m}M${duration.ISO}`
-            nextUnit = 'hour'
-          } else {
-            duration.m = v
-            duration.ISO = `${duration.m}M${duration.ISO}`
-            nextUnit = ''
-          }
-          break
-        case 'hour':
-          r = Math.floor(v / 24)
-          if (r >= 1) {
-            duration.H = v % 24
-            duration.ISO = `T${duration.H}H${duration.ISO}`
-            nextUnit = 'day'
-          } else {
-            duration.H = v
-            duration.ISO = `T${duration.H}H${duration.ISO}`
-            nextUnit = ''
-          }
-          break
-        case 'day': // average number of day per month 30.416 in a year
-          r = Math.floor(v / 30.416) // is it ok?
-          if (r >= 1) {
-            duration.D = Math.floor(v % 30.416)
-            duration.ISO = `${duration.D}D${duration.ISO}`
-            nextUnit = 'month'
-          } else {
-            duration.D = v
-            duration.ISO = `${duration.D}D${duration.ISO}`
-            nextUnit = ''
-          }
-          break
-        // case 'week': // add week?
-        //   break
-        case 'month':
-          r = Math.floor(v / 12)
-          if (r >= 1) {
-            duration.M = v % 12
-            duration.ISO = `${duration.M}M${duration.ISO}`
-            nextUnit = 'year'
-          } else {
-            duration.M = v
-            duration.ISO = `${duration.M}M${duration.ISO}`
-            nextUnit = ''
-          }
-          break
-        case 'year':
-          duration.y = v
-          duration.ISO = `${duration.y}Y${duration.ISO}`
-          nextUnit = ''
-          break
-        default:
-          r = 0
-          nextUnit = ''
-          break
-      }
-      return [r, nextUnit]
+    const TIME_CONSTANT = {
+      seconde: { u: 60, s: 'S', next: 'minute' },
+      minute: { u: 60, s: 'M', next: 'hour' },
+      hour: { u: 24, s: 'H', next: 'day' },
+      day: { u: 30.417, s: 'D', next: 'month' }, // average number of day per month 30.417 in a year
+      month: { u: 12, s: 'M', next: 'year' },
+      year: { u: 1, s: 'Y', next: '' }
     }
-
-    /**
-    * return the desired format of duration
-    * @param  {string} token a string describing the desired format
-    * @return {string} return duration format
-    */
-
-    // function format(token) {
-    // }
 
     const duration = {
       ISO: '',
@@ -116,12 +29,40 @@ export default (o, c) => {
       M: 0,
       y: 0
     }
+    /**
+    * convert a number to his time unit
+    * @param  {number} v a number
+    * @param  {string} u unit of time of the number
+    * @return {array[r, nextUnit]} return a value and his time unit to parse
+    */
+    function parse(v, u) { // way shorter
+      let nextUnit = ''
+      const r = Math.floor(v / TIME_CONSTANT[u].u) // is it ok?
+      if (r >= 1) {
+        duration.D = TIME_CONSTANT[u].u === 1 ? v : Math.floor(v % TIME_CONSTANT[u].u)
+        nextUnit = TIME_CONSTANT[u].next
+      } else {
+        duration.D = v
+        nextUnit = ''
+      }
+      duration.ISO = u === 'hour' ? `T${duration.D}${TIME_CONSTANT[u].s}${duration.ISO}` : `${duration.D}${TIME_CONSTANT[u].s}${duration.ISO}`
+      return [r, nextUnit]
+    }
+
+    /**
+    * return the desired format of duration
+    * @param  {string} token a string describing the desired format
+    * @return {string} return duration format
+    */
+    // format function () {
+    //
+    // }
     // duration.format = format
 
     let rest = value
     let u = unit
     while (u !== '') {
-      [rest, u] = parse(rest, u, duration)
+      [rest, u] = parse(rest, u)
     }
     duration.ISO = `P${duration.ISO}`
 
