19affc84bbec84bad840e310b390db5f92b2499a
Guillaume Briday <guillaumebriday@gmail.com>
2019-07-16 10:19:34 +0200

    fix: Return null in toJSON if not valid (#633)
    * fix: toJSON returns null if not valid
    
    * fix: typo in locale
    
    * fix: adding test on toJSON

--- test/display.test.js
+++ test/display.test.js
@@ -248,6 +248,8 @@ it('As Javascript Date -> toDate', () => {
 
 it('As JSON -> toJSON', () => {
   expect(dayjs().toJSON()).toBe(moment().toJSON())
+  expect(dayjs('otherString').toJSON()).toBe(moment('otherString').toJSON())
+  expect(dayjs('otherString').toJSON()).toBe(null)
 })
 
 it('As ISO 8601 String -> toISOString e.g. 2013-02-04T22:44:30.652Z', () => {
