0e45b0ace9e1a22c679fa351d4391e306ca9b25d
Yue Yang <g1enyy0ung@gmail.com>
2019-08-19 13:38:45 +0800

    fix: Handle locale in WeekOfYear plugin (#658)

--- src/plugin/weekOfYear/index.js
+++ src/plugin/weekOfYear/index.js
@@ -6,13 +6,22 @@ export default (o, c, d) => {
     if (week !== null) {
       return this.add((week - this.week()) * 7, 'day')
     }
+
+    const weekStart = this.$locale().weekStart || 0
+
     // d(this) clone is for badMutable plugin
     const endOfYear = d(this).endOf(Y)
-    if (endOfYear.day() !== 6 && this.month() === 11 && (31 - this.date()) <= endOfYear.day()) {
+    if (
+      weekStart === 0 &&
+      endOfYear.day() !== 6 &&
+      this.month() === 11 &&
+      31 - this.date() <= endOfYear.day()
+    ) {
       return 1
     }
+
     const startOfYear = d(this).startOf(Y)
-    const compareDay = startOfYear.subtract(startOfYear.day(), D).subtract(1, MS)
+    const compareDay = startOfYear.subtract(startOfYear.day() - weekStart, D).subtract(1, MS)
     const diffInWeek = this.diff(compareDay, W, true)
     return Math.ceil(diffInWeek)
   }
