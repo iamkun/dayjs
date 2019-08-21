6f3a6c8e5925e143adcffc04c3177e9ea62f1f09
iamkun <kunhello@outlook.com>
2019-08-21 17:25:46 +0800

    use constant

--- src/plugin/setUTCOffset/index.js
+++ src/plugin/setUTCOffset/index.js
@@ -1,3 +1,5 @@
+import { MILLISECONDS_A_MINUTE, MIN } from '../../constant'
+
 export default (option, Dayjs) => {
   const proto = Dayjs.prototype
   const oldParse = proto.parse
@@ -11,7 +13,7 @@ export default (option, Dayjs) => {
 
   proto.valueOf = function () {
     const addedOffset = !this.$utils().u(this.$offset) ? (this.$offset || 0) + localOffset : 0
-    return this.$d.valueOf() - (addedOffset * 60000)
+    return this.$d.valueOf() - (addedOffset * MILLISECONDS_A_MINUTE)
   }
 
   const oldUtcOffset = proto.utcOffset
@@ -28,7 +30,7 @@ export default (option, Dayjs) => {
     }
     const offset = Math.abs(input) <= 16 ? input * 60 : input
     this.$offset = offset
-    return this.add(offset + localOffset, 'minute')
+    return this.add(offset + localOffset, MIN)
   }
 
   // todo toString valueOf toDate shoud minus back to original date
