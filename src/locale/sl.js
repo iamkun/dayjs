505f23e74f3d33c86821e5147b0f7671cd19b0c9
Janez Kranjc <janez.kranjc@gmail.com>
2019-09-01 22:02:31 +0200

    Fix SL abbreviation for month may

--- src/locale/sl.js
+++ src/locale/sl.js
@@ -6,7 +6,7 @@ const locale = {
   months: 'januar_februar_marec_april_maj_junij_julij_avgust_september_oktober_november_december'.split('_'),
   weekStart: 1,
   weekdaysShort: 'ned._pon._tor._sre._čet._pet._sob.'.split('_'),
-  monthsShort: 'jan._feb._mar._apr._maj._jun._jul._avg._sep._okt._nov._dec.'.split('_'),
+  monthsShort: 'jan._feb._mar._apr._maj_jun._jul._avg._sep._okt._nov._dec.'.split('_'),
   weekdaysMin: 'ne_po_to_sr_če_pe_so'.split('_'),
   ordinal: n => n,
   formats: {
