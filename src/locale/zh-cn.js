d3ccebd5b80f5542547c2bd5e36beb3d35a594b7
iamkun <kunhello@outlook.com>
2019-11-15 15:55:13 +0800

    Update zh-cn.js

--- src/locale/zh-cn.js
+++ src/locale/zh-cn.js
@@ -43,9 +43,21 @@ const locale = {
     y: '1 年',
     yy: '%d 年'
   },
-  meridiem: hour => {
-    // 可选, AM/PM
-    return hour > 12 ? '下午' : '上午'
+  meridiem: (hour, minute) => {
+    const hm = hour * 100 + minute
+    if (hm < 600) {
+        return '凌晨'
+    } else if (hm < 900) {
+        return '早上'
+    } else if (hm < 1130) {
+        return '上午'
+    } else if (hm < 1230) {
+        return '中午'
+    } else if (hm < 1800) {
+        return '下午'
+    } else {
+        return '晚上'
+    }
   }
 }
 
