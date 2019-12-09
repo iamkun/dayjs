aa0f210a1e3c4f6aba61c3b96f9eb445b43a33f0
iamkun <kunhello@outlook.com>
2019-12-09 21:35:15 +0800

    fix: Update LocaleData plugin to support dayjs().longDateFormat() (#734)
    fix #680

--- test/plugin/localeData.test.js
+++ test/plugin/localeData.test.js
@@ -2,8 +2,10 @@ import MockDate from 'mockdate'
 import moment from 'moment'
 import dayjs from '../../src'
 import localeData from '../../src/plugin/localeData'
+import localizedFormat from '../../src/plugin/localizedFormat'
 import '../../src/locale/zh-cn'
 
+dayjs.extend(localizedFormat)
 dayjs.extend(localeData)
 
 beforeEach(() => {
@@ -14,7 +16,7 @@ afterEach(() => {
   MockDate.reset()
 })
 
-it('instance localeData', () => {
+it('Instance localeData', () => {
   const d = dayjs()
   const m = moment()
   const dayjsLocaleData = dayjs().localeData()
@@ -28,9 +30,14 @@ it('instance localeData', () => {
   expect(dayjsLocaleData.weekdaysMin()).toEqual(momentLocaleData.weekdaysMin())
   expect(dayjsLocaleData.weekdaysShort(d)).toBe(momentLocaleData.weekdaysShort(m))
   expect(dayjsLocaleData.weekdaysShort()).toEqual(momentLocaleData.weekdaysShort())
+  const longDateFormats = ['LT', 'LTS', 'L', 'LL', 'LLL', 'LLLL']
+  longDateFormats.forEach((f) => {
+    expect(dayjsLocaleData.longDateFormat(f)).toEqual(momentLocaleData.longDateFormat(f))
+  })
 })
 
-it('global localeData', () => {
+
+it('Global localeData', () => {
   dayjs.locale('zh-cn')
   moment.locale('zh-cn')
   let dayjsLocaleData = dayjs.localeData()
