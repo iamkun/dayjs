7d46d77cb92be972f7b775969c0df75156cfcd4a
iamkun <kunhello@outlook.com>
2020-01-06 16:30:32 +0800

    fix: Update ru && uk locale file to support relativeTime with plural

---
+++ test/locale/uk.test.js
@@ -0,0 +1,36 @@
+import moment from 'moment'
+import MockDate from 'mockdate'
+import dayjs from '../../src'
+import relativeTime from '../../src/plugin/relativeTime'
+import '../../src/locale/uk'
+
+dayjs.extend(relativeTime)
+
+beforeEach(() => {
+  MockDate.set(new Date())
+})
+
+afterEach(() => {
+  MockDate.reset()
+})
+
+it('RelativeTime: Time from X', () => {
+  const T = [
+    [44.4, 'second'], // a few seconds
+    [89.5, 'second'], // a minute
+    [43, 'minute'], // 44 minutes
+    [21, 'hour'], // 21 hours
+    [25, 'day'], // 25 days
+    [10, 'month'], // 2 month
+    [18, 'month'] // 2 years
+  ]
+
+  T.forEach((t) => {
+    dayjs.locale('uk')
+    moment.locale('uk')
+    expect(dayjs().from(dayjs().add(t[0], t[1])))
+      .toBe(moment().from(moment().add(t[0], t[1])))
+    expect(dayjs().from(dayjs().add(t[0], t[1]), true))
+      .toBe(moment().from(moment().add(t[0], t[1]), true))
+  })
+})
