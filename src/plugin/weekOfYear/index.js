e0e26c045e8a9c9e9943e32858a5481ec9e56588
iamkun <kunhello@outlook.com>
2020-01-11 15:47:15 +0800

    chore update

--- src/plugin/weekOfYear/index.js
+++ src/plugin/weekOfYear/index.js
@@ -7,9 +7,6 @@ export default (o, c) => {
       return this.add((week - this.week()) * 7, D)
     }
     const yearStart = this.$locale().yearStart || 1
-    const yearStartDay = this.startOf(Y).date(yearStart)
-    const yearStartWeek = yearStartDay.startOf(W).subtract(1, MS)
-    const diffInWeek = this.diff(yearStartWeek, W, true)
     if (this.month() === 11 && this.date() > 25) {
       const nextYearStartDay = this.startOf(Y).add(1, Y).date(yearStart)
       const thisEndOfWeek = this.endOf(W)
@@ -17,6 +14,9 @@ export default (o, c) => {
         return 1
       }
     }
+    const yearStartDay = this.startOf(Y).date(yearStart)
+    const yearStartWeek = yearStartDay.startOf(W).subtract(1, MS)
+    const diffInWeek = this.diff(yearStartWeek, W, true)
     if (diffInWeek < 0) {
       return this.startOf('week').week()
     }
