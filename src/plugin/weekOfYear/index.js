d2bd8ffc6ed69c89643e8600dad8befb5fb892ea
iamkun <kunhello@outlook.com>
2020-01-11 15:31:12 +0800

    chore update

--- src/plugin/weekOfYear/index.js
+++ src/plugin/weekOfYear/index.js
@@ -1,35 +1,26 @@
 import { MS, Y, D, W } from '../../constant'
 
-const checkSpecialYear = (ins) => {
-  const { yearStart } = ins.$locale()
-  if (!yearStart) return false
-  const realStartOfYear = ins.startOf(Y).date(yearStart)
-  const dayOfRealStartOfYear = realStartOfYear.day() || 7
-  return dayOfRealStartOfYear < yearStart
-}
-
-export default (o, c, d) => {
+export default (o, c) => {
   const proto = c.prototype
   proto.week = function (week = null) {
     if (week !== null) {
-      return this.add((week - this.week()) * 7, 'day')
+      return this.add((week - this.week()) * 7, D)
     }
-    const weekStart = this.$locale().weekStart || 0
-    const { yearStart } = this.$locale()
-    let startOfYear = d(this).startOf(Y)
-    const isSpecialWeekStart = checkSpecialYear(this)
-    if (isSpecialWeekStart) {
-      startOfYear = startOfYear.date(yearStart)
+    const yearStart = this.$locale().yearStart || 1
+    const yearStartDay = this.startOf(Y).date(yearStart)
+    const yearStartWeek = yearStartDay.startOf(W).subtract(1, MS)
+    const diffInWeek = this.diff(yearStartWeek, W, true)
+    if (this.month() === 11 && this.date() > 25) {
+      const nextYearStartDay = this.startOf(Y).add(1, Y).date(yearStart)
+      const thisEndOfWeek = this.endOf(W)
+      if (nextYearStartDay.isBefore(thisEndOfWeek)) {
+        return 1
+      }
     }
-    const compareDay = startOfYear.subtract(startOfYear.day() - weekStart, D).subtract(1, MS)
-    const diffInWeek = this.diff(compareDay, W, true)
-    if (diffInWeek < 0) return d(this).startOf(Y).subtract(1, MS).week()
-    const result = Math.ceil(diffInWeek)
-    if (result > 52) {
-      const nextYearDay = this.add(1, 'month')
-      return checkSpecialYear(nextYearDay) ? result : 1
+    if (diffInWeek < 0) {
+      return this.startOf('week').week()
     }
-    return result
+    return Math.ceil(diffInWeek)
   }
 
   proto.weeks = function (week = null) {
