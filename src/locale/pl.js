d3724758bb27d5b17587b995ba14e7e80dcd1151
iamkun <kunhello@outlook.com>
2020-01-19 16:24:39 +0800

    fix: Update pl locale fusional config

--- src/locale/pl.js
+++ src/locale/pl.js
@@ -1,5 +1,27 @@
 import dayjs from 'dayjs'
 
+function plural(n) {
+  return (n % 10 < 5) && (n % 10 > 1) && ((~~(n / 10) % 10) !== 1) // eslint-disable-line
+}
+/* eslint-disable */ 
+function translate(number, withoutSuffix, key) {
+  const result = `${number} `
+  switch (key) {
+    case 'm':
+      return withoutSuffix ? 'minuta' : 'minutę'
+    case 'mm':
+      return result + (plural(number) ? 'minuty' : 'minut')
+    case 'h':
+      return withoutSuffix ? 'godzina' : 'godzinę'
+    case 'hh':
+      return result + (plural(number) ? 'godziny' : 'godzin')
+    case 'MM':
+      return result + (plural(number) ? 'miesiące' : 'miesięcy')
+    case 'yy':
+      return result + (plural(number) ? 'lata' : 'lat')
+  }
+}
+/* eslint-enable */
 const locale = {
   name: 'pl',
   weekdays: 'Niedziela_Poniedziałek_Wtorek_Środa_Czwartek_Piątek_Sobota'.split('_'),
@@ -11,18 +33,18 @@ const locale = {
   weekStart: 1,
   relativeTime: {
     future: 'za %s',
-    past: 'po %s',
+    past: '%s temu',
     s: 'kilka sekund',
-    m: 'minuta',
-    mm: '%d minut',
-    h: 'godzina',
-    hh: '%d godzin',
-    d: 'dzień',
+    m: translate,
+    mm: translate,
+    h: translate,
+    hh: translate,
+    d: '1 dzień',
     dd: '%d dni',
     M: 'miesiąc',
-    MM: '%d miesięcy',
+    MM: translate,
     y: 'rok',
-    yy: '%d lat'
+    yy: translate
   },
   formats: {
     LT: 'HH:mm',
