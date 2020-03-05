a86b33e6c096416e3cd626a2daf08fb16965f31d
Zhi Yin <Z.Yin.MD@gmail.com>
2020-03-05 09:40:21 -0500

    fix spacing in relative time for Chinese locales
    This affects the zh-cn, zh-hk, and zh-tw locales.
    In Chinese, there is no whitespace between numbers and units.
    For instance, "3 days" is "3天", not "3 天"

--- src/locale/zh-cn.js
+++ src/locale/zh-cn.js
@@ -34,16 +34,16 @@ const locale = {
     future: '%s内',
     past: '%s前',
     s: '几秒',
-    m: '1 分钟',
-    mm: '%d 分钟',
-    h: '1 小时',
-    hh: '%d 小时',
-    d: '1 天',
-    dd: '%d 天',
-    M: '1 个月',
-    MM: '%d 个月',
-    y: '1 年',
-    yy: '%d 年'
+    m: '1分钟',
+    mm: '%d分钟',
+    h: '1小时',
+    hh: '%d小时',
+    d: '1天',
+    dd: '%d天',
+    M: '1个月',
+    MM: '%d个月',
+    y: '1年',
+    yy: '%d年'
   },
   meridiem: (hour, minute) => {
     const hm = (hour * 100) + minute
