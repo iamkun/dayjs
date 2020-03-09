db94354889bb1e62fd5abb655b30e02e8f489b6b
Zhi Yin <Z.Yin.MD@gmail.com>
2020-03-09 14:19:05 -0400

    improve zh testing: minimize dependencies

--- test/locale/zh.test.js
+++ test/locale/zh.test.js
@@ -1,33 +1,20 @@
-import moment from 'moment'
-import MockDate from 'mockdate'
 import dayjs from '../../src'
 import advancedFormat from '../../src/plugin/advancedFormat'
 import weekOfYear from '../../src/plugin/weekOfYear'
-
 import '../../src/locale/zh.js'
+import '../../src/locale/zh-cn.js'
 
 dayjs.extend(advancedFormat).extend(weekOfYear)
 
-beforeEach(() => {
-  MockDate.set(new Date())
-})
-
-afterEach(() => {
-  MockDate.reset()
-})
-
-const dayjsObj = dayjs().locale('zh')
-
-test('Meridiem', () => {
-  const momentObj = moment().locale('zh-cn')
-  for (let i = 0; i <= 24; i += 1) {
-    // prettier conflicts with eslint, so
-    // prettier-ignore
-    expect(dayjsObj.add(i, 'hour').format('A'))
-      .toEqual(momentObj.clone().add(i, 'hour').format('A'))
-  }
-})
+const zh = dayjs().locale('zh')
+const zhCN = dayjs().locale('zh-cn')
 
 test('ordinal', () => {
-  expect(dayjsObj.format('wo')).toEqual(`第${dayjsObj.format('w')}周`)
+  expect(zh.format('wo')).toEqual(`第${zh.format('w')}周`)
+})
+
+test('Meridiem', () => {
+  for (let i = 0; i <= 24; i += 1) {
+    expect(zh.add(i, 'hour').format('A')).toBe(zhCN.add(i, 'hour').format('A'))
+  }
 })
