e521c73551e972c0825237c6f396afaf87e6d2b7
iamkun <kunhello@outlook.com>
2020-04-07 18:24:08 +0800

    init test2

--- test/plugin/duration/creating.test.js
+++ test/plugin/duration/creating.test.js
@@ -13,7 +13,9 @@ afterEach(() => {
 })
 
 describe('Creating from two argument', () => {
-  it('Creating from two argument', () => {
+  it('two argument will bubble up to the next', () => {
+    expect(dayjs.duration(59, 'seconds').toISOString()).toBe('PT59S')
+    expect(dayjs.duration(60, 'seconds').toISOString()).toBe('P1M')
     expect(dayjs.duration(13213, 'seconds').toISOString()).toBe('PT3H40M13S')
   })
 })
