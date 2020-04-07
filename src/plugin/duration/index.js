90568f847e1d797412df80644b055416a242580a
iamkun <kunhello@outlook.com>
2020-04-07 18:55:47 +0800

    add test

--- src/plugin/duration/index.js
+++ src/plugin/duration/index.js
@@ -15,54 +15,58 @@ const unitToMS = {
 }
 class Duration {
   constructor(input, unit) {
+    this.$d = {}
     if (unit) {
       return new Duration(input * unitToMS[unit])
     }
     if (typeof input === 'number') {
-      this.milliseconds = input
+      this.$d.milliseconds = input
       this.parseFromMilliseconds()
       return this
     }
     if (typeof input === 'object') {
       Object.keys(input).forEach((k) => {
-        this[k] = input[k]
+        this.$d[k] = input[k]
       })
-      if (input.weeks) {
-        this.days += input.weeks * 7
-      }
       return this
     }
     if (typeof input === 'string') {
       const d = input.match(durationRegex)
       if (d) {
-        [,, this.years, this.months, , this.days, this.hours, this.minutes, this.seconds] = d
+        [,,
+          this.$d.years, this.$d.months,,
+          this.$d.days, this.$d.hours, this.$d.minutes, this.$d.seconds] = d
       }
       return this
     }
   }
 
   parseFromMilliseconds() {
-    let { milliseconds } = this
-    this.years = Math.floor(milliseconds / MILLISECONDS_A_YEAR)
+    let { milliseconds } = this.$d
+    this.$d.years = Math.floor(milliseconds / MILLISECONDS_A_YEAR)
     milliseconds %= MILLISECONDS_A_YEAR
-    this.months = Math.floor(milliseconds / MILLISECONDS_A_MONTH)
+    this.$d.months = Math.floor(milliseconds / MILLISECONDS_A_MONTH)
     milliseconds %= MILLISECONDS_A_MONTH
-    this.days = Math.floor(milliseconds / MILLISECONDS_A_DAY)
+    this.$d.days = Math.floor(milliseconds / MILLISECONDS_A_DAY)
     milliseconds %= MILLISECONDS_A_DAY
-    this.hours = Math.floor(milliseconds / MILLISECONDS_A_HOUR)
+    this.$d.hours = Math.floor(milliseconds / MILLISECONDS_A_HOUR)
     milliseconds %= MILLISECONDS_A_HOUR
-    this.minutes = Math.floor(milliseconds / MILLISECONDS_A_MINUTE)
+    this.$d.minutes = Math.floor(milliseconds / MILLISECONDS_A_MINUTE)
     milliseconds %= MILLISECONDS_A_MINUTE
-    this.seconds = milliseconds / MILLISECONDS_A_SECOND
+    this.$d.seconds = milliseconds / MILLISECONDS_A_SECOND
   }
 
   toISOString() {
-    const Y = this.years ? `${this.years}Y` : ''
-    const M = this.months ? `${this.months}M` : ''
-    const D = this.days ? `${this.days}D` : ''
-    const H = this.hours ? `${this.hours}H` : ''
-    const m = this.minutes ? `${this.minutes}M` : ''
-    const S = this.seconds ? `${this.seconds}S` : ''
+    const Y = this.$d.years ? `${this.$d.years}Y` : ''
+    const M = this.$d.months ? `${this.$d.months}M` : ''
+    let days = this.$d.days || 0
+    if (this.$d.weeks) {
+      days += this.$d.weeks * 7
+    }
+    const D = days ? `${days}D` : ''
+    const H = this.$d.hours ? `${this.$d.hours}H` : ''
+    const m = this.$d.minutes ? `${this.$d.minutes}M` : ''
+    const S = this.$d.seconds ? `${this.$d.seconds}S` : ''
     const T = (H || M || S) ? 'T' : ''
     return `P${Y}${M}${D}${T}${H}${m}${S}`
   }
@@ -72,15 +76,31 @@ class Duration {
   }
 
   as(unit) {
-    return this.milliseconds / unitToMS[unit]
+    return this.$d.milliseconds / unitToMS[unit]
   }
   get(unit) {
-    return this[unit]
+    return this.$d[unit]
   }
   // isDuration
   // add
   // subtract
   // negative duration
+  milliseconds() { return this.get('milliseconds') }
+  asMilliseconds() { return this.as('milliseconds') }
+  seconds() { return this.get('seconds') }
+  asSeconds() { return this.get('seconds') }
+  minutes() { return this.get('minutes') }
+  asMinutes() { return this.get('minutes') }
+  hours() { return this.get('hours') }
+  asHours() { return this.get('hours') }
+  days() { return this.get('days') }
+  asDays() { return this.get('days') }
+  weeks() { return this.get('weeks') }
+  asWeeks() { return this.get('weeks') }
+  months() { return this.get('months') }
+  asMonths() { return this.get('months') }
+  years() { return this.get('years') }
+  asYears() { return this.get('years') }
 }
 export default (option, Dayjs, dayjs) => {
   // const proto = Dayjs.prototype
