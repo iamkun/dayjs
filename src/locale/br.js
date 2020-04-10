a2a66720abb788a8f1cffbfd0929b35579f29c72
Chamie <mail@chamie.fr>
2020-04-10 04:49:22 +0200

    fix: Update Breton locale (br) meridiem config (#856)

--- src/locale/br.js
+++ src/locale/br.js
@@ -3,11 +3,11 @@ import dayjs from 'dayjs'
 
 const locale = {
   name: 'br',
-  weekdays: "Sul_Lun_Meurzh_Merc'her_Yaou_Gwener_Sadorn".split('_'),
-  months: "Genver_C'hwevrer_Meurzh_Ebrel_Mae_Mezheven_Gouere_Eost_Gwengolo_Here_Du_Kerzu".split('_'),
+  weekdays: 'Sul_Lun_Meurzh_Mercʼher_Yaou_Gwener_Sadorn'.split('_'),
+  months: 'Genver_Cʼhwevrer_Meurzh_Ebrel_Mae_Mezheven_Gouere_Eost_Gwengolo_Here_Du_Kerzu'.split('_'),
   weekStart: 1,
   weekdaysShort: 'Sul_Lun_Meu_Mer_Yao_Gwe_Sad'.split('_'),
-  monthsShort: "Gen_C'hwe_Meu_Ebr_Mae_Eve_Gou_Eos_Gwe_Her_Du_Ker".split('_'),
+  monthsShort: 'Gen_Cʼhwe_Meu_Ebr_Mae_Eve_Gou_Eos_Gwe_Her_Du_Ker'.split('_'),
   weekdaysMin: 'Su_Lu_Me_Mer_Ya_Gw_Sa'.split('_'),
   ordinal: n => n,
   formats: {
@@ -17,10 +17,10 @@ const locale = {
     LL: 'D [a viz] MMMM YYYY',
     LLL: 'D [a viz] MMMM YYYY h[e]mm A',
     LLLL: 'dddd, D [a viz] MMMM YYYY h[e]mm A'
-  }
+  },
+  meridiem: hour => (hour < 12 ? 'a.m.' : 'g.m.') // a-raok merenn | goude merenn
 }
 
 dayjs.locale(locale, null, true)
 
 export default locale
-
