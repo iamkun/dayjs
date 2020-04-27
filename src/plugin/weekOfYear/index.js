2977438458542573a4500e21f7ba5d1f8442960e
iamkun <kunhello@outlook.com>
2020-04-27 13:32:03 +0800

    fix: Fix WeekOfYear plugin bug while using BadMutable plugin (#884)

--- src/plugin/weekOfYear/index.js
+++ src/plugin/weekOfYear/index.js
@@ -1,6 +1,6 @@
 import { MS, Y, D, W } from '../../constant'
 
-export default (o, c) => {
+export default (o, c, d) => {
   const proto = c.prototype
   proto.week = function (week = null) {
     if (week !== null) {
@@ -8,17 +8,18 @@ export default (o, c) => {
     }
     const yearStart = this.$locale().yearStart || 1
     if (this.month() === 11 && this.date() > 25) {
-      const nextYearStartDay = this.startOf(Y).add(1, Y).date(yearStart)
-      const thisEndOfWeek = this.endOf(W)
+      // d(this) is for badMutable
+      const nextYearStartDay = d(this).startOf(Y).add(1, Y).date(yearStart)
+      const thisEndOfWeek = d(this).endOf(W)
       if (nextYearStartDay.isBefore(thisEndOfWeek)) {
         return 1
       }
     }
-    const yearStartDay = this.startOf(Y).date(yearStart)
+    const yearStartDay = d(this).startOf(Y).date(yearStart)
     const yearStartWeek = yearStartDay.startOf(W).subtract(1, MS)
     const diffInWeek = this.diff(yearStartWeek, W, true)
     if (diffInWeek < 0) {
-      return this.startOf('week').week()
+      return d(this).startOf('week').week()
     }
     return Math.ceil(diffInWeek)
   }
