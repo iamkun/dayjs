7c9430c9a17308df8ee25bf81d91a452c88fde87
Valeriia Braterska <val.braterskaya@gmail.com>
2020-05-11 23:26:16 +0300

    fixed monthFormat uk locale

--- src/locale/uk.js
+++ src/locale/uk.js
@@ -1,6 +1,14 @@
 // Ukrainian [uk]
 import dayjs from 'dayjs'
 
+const monthFormat = 'січня_лютого_березня_квітня_травня_червня_липня_серпня_вересня_жовтня_листопада_грудня'.split('_')
+const monthStandalone = 'січень_лютий_березень_квітень_травень_червень_липень_серпень_вересень_жовтень_листопад_грудень'.split('_')
+
+const monthShortFormat = 'сiч_лют_бер_квiт_трав_черв_лип_серп_вер_жовт_лист_груд'.split('_')
+const monthShortStandalone = 'сiч_лют_бер_квiт_трав_черв_лип_серп_вер_жовт_лист_груд'.split('_')
+
+const MONTHS_IN_FORMAT = /D[oD]?(\[[^[\]]*\]|\s)+MMMM?/
+
 function plural(word, num) {
   const forms = word.split('_')
   return num % 10 === 1 && num % 100 !== 11 ? forms[0] : (num % 10 >= 2 && num % 10 <= 4 && (num % 100 < 10 || num % 100 >= 20) ? forms[1] : forms[2]) // eslint-disable-line
@@ -28,8 +36,18 @@ const locale = {
   weekdays: 'неділя_понеділок_вівторок_середа_четвер_п’ятниця_субота'.split('_'),
   weekdaysShort: 'ндл_пнд_втр_срд_чтв_птн_сбт'.split('_'),
   weekdaysMin: 'нд_пн_вт_ср_чт_пт_сб'.split('_'),
-  months: 'січень_лютий_березень_квітень_травень_червень_липень_серпень_вересень_жовтень_листопад_грудень'.split('_'),
-  monthsShort: 'сiч_лют_бер_квiт_трав_черв_лип_серп_вер_жовт_лист_груд'.split('_'),
+  months: (dayjsInstance, format) => {
+    if (MONTHS_IN_FORMAT.test(format)) {
+      return monthFormat[dayjsInstance.month()]
+    }
+    return monthStandalone[dayjsInstance.month()]
+  },
+  monthsShort: (dayjsInstance, format) => {
+    if (MONTHS_IN_FORMAT.test(format)) {
+      return monthShortFormat[dayjsInstance.month()]
+    }
+    return monthShortStandalone[dayjsInstance.month()]
+  },
   weekStart: 1,
   relativeTime: {
     future: 'за %s',
