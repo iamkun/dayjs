71fabcd441a45e74bbbce5fd4fb7661b1f2d626c
Valeriia Braterska <val.braterskaya@gmail.com>
2020-05-22 11:05:22 +0300

    fixed month short format

--- src/locale/uk.js
+++ src/locale/uk.js
@@ -4,9 +4,6 @@ import dayjs from 'dayjs'
 const monthFormat = 'січня_лютого_березня_квітня_травня_червня_липня_серпня_вересня_жовтня_листопада_грудня'.split('_')
 const monthStandalone = 'січень_лютий_березень_квітень_травень_червень_липень_серпень_вересень_жовтень_листопад_грудень'.split('_')
 
-const monthShortFormat = 'сiч_лют_бер_квiт_трав_черв_лип_серп_вер_жовт_лист_груд'.split('_')
-const monthShortStandalone = 'сiч_лют_бер_квiт_трав_черв_лип_серп_вер_жовт_лист_груд'.split('_')
-
 const MONTHS_IN_FORMAT = /D[oD]?(\[[^[\]]*\]|\s)+MMMM?/
 
 function plural(word, num) {
@@ -40,22 +37,13 @@ const months = (dayjsInstance, format) => {
 months.s = monthStandalone
 months.f = monthFormat
 
-const monthsShort = (dayjsInstance, format) => {
-  if (MONTHS_IN_FORMAT.test(format)) {
-    return monthShortFormat[dayjsInstance.month()]
-  }
-  return monthShortStandalone[dayjsInstance.month()]
-}
-monthsShort.s = monthShortStandalone
-monthsShort.f = monthShortFormat
-
 const locale = {
   name: 'uk',
   weekdays: 'неділя_понеділок_вівторок_середа_четвер_п’ятниця_субота'.split('_'),
   weekdaysShort: 'ндл_пнд_втр_срд_чтв_птн_сбт'.split('_'),
   weekdaysMin: 'нд_пн_вт_ср_чт_пт_сб'.split('_'),
   months,
-  monthsShort,
+  monthsShort:'сiч_лют_бер_квiт_трав_черв_лип_серп_вер_жовт_лист_груд'.split('_'),
   weekStart: 1,
   relativeTime: {
     future: 'за %s',
