77c70a0a45ffbde59542c02e529f8001150030ae
stoicsleuth <tarashnkr321@gmail.com>
2020-08-06 01:18:54 +0530

    Added typedefs in esm build

--- build/esm.js
+++ build/esm.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 const util = require('util')
+const { ncp } = require('ncp')
 
 const { promisify } = util
 
@@ -14,6 +15,10 @@ const localeDir = path.join(process.env.PWD, 'esm/locale');
       const readFile = await promisify(fs.readFile)(filePath, 'utf8')
       const result = readFile.replace("'dayjs'", "'../index'")
       await promisify(fs.writeFile)(filePath, result, 'utf8')
+    })
+
+    ncp('./types/', './esm', (err) => {
+      if (err) { throw err }
     })
   } catch (e) {
     console.error(e) // eslint-disable-line no-console
