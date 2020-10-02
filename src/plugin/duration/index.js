57fedbeaaada4d8755841d959a40012b4a969b42
stebogit <stefano.borghi@gmail.com>
2020-10-02 08:12:37 -0700

    DRY

--- src/plugin/duration/index.js
+++ src/plugin/duration/index.js
@@ -180,8 +180,9 @@ export default (option, Dayjs, dayjs) => {
   const oldAdd = Dayjs.prototype.add
   Dayjs.prototype.add = function (addition, units) {
     if (isDuration(addition)) {
-      return oldAdd.bind(this)(addition.asMilliseconds(), 'ms')
+      addition = addition.asMilliseconds();
+      units = 'ms';
     }
-    return oldAdd.bind(this)(addition, units)
+    return oldAdd.bind(this)(addition, units);
   }
 }
