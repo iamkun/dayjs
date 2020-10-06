f91bde0d5c3f005c3a8271715feb81ce4cf040b2
Mocha-- <xibowangmelb@gmail.com>
2020-10-06 21:53:44 +1100

    change(localData): export function declarations

--- types/plugin/localeData.d.ts
+++ types/plugin/localeData.d.ts
@@ -6,5 +6,14 @@ export = plugin
 declare module 'dayjs' {
   interface Dayjs {
     localeData(): any
-  }
+  }  
+  
+  type DayNamesTuple = [string, string, string, string, string, string, string];
+  type MonthNamesTuple = [string, string, string, string, string, string, string, string, string, string, string, string];
+
+  export function weekdays(): DayNamesTuple;
+  export function weekdaysShort(): DayNamesTuple;
+  export function weekdaysMin(): DayNamesTuple;
+  export function monthsShort(): MonthNamesTuple;
+  export function months(): MonthNamesTuple;
 }
