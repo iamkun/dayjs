25199794405372936b19773d2bd8f84f4ae987b2
Vignesh Murugan <vignesh328@gmail.com>
2020-10-11 16:31:54 +0530

    #1118 - Partial Locale update added

--- src/plugin/updateLocale/index.js
+++ src/plugin/updateLocale/index.js
@@ -5,7 +5,7 @@ export default (option, Dayjs, dayjs) => {
     if (!localeConfig) return
     const customConfigKeys = customConfig ? Object.keys(customConfig) : []
     customConfigKeys.forEach((c) => {
-      localeConfig[c] = customConfig[c]
+      localeConfig[c] = typeof localeConfig[c] === 'object' ? Object.assign({} , localeConfig[c], customConfig[c]): customConfig[c]
     })
     return localeConfig // eslint-disable-line consistent-return
   }
