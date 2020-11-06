3af36ca02adecd4d1215fb814c7b1b34bef7b5b4
iamkun <kunhello@outlook.com>
2020-11-06 10:47:37 +0800

    fix: fix utc plugin diff edge case

--- test/plugin/utc.test.js
+++ test/plugin/utc.test.js
@@ -266,3 +266,7 @@ it('utc keepLocalTime', () => {
   expect(dd).toEqual(dm)
   expect(vd).toEqual(vm)
 })
+
+it('utc diff undefined edge case', () => {
+  expect(dayjs().diff(undefined, 'seconds')).toBeDefined()
+})
