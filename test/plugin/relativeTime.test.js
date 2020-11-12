ef1979ce85c61fe2d759ef3c37cb6aaf2358094f
Ryan Nixon Salim <ryan.nixon.salim@gmail.com>
2020-11-13 02:18:55 +0700

    fix: Add function handling for relativeTime.future and relativeTime.past (#1197)

--- test/plugin/relativeTime.test.js
+++ test/plugin/relativeTime.test.js
@@ -3,6 +3,7 @@ import moment from 'moment'
 import dayjs from '../../src'
 import * as C from '../../src/constant'
 import relativeTime from '../../src/plugin/relativeTime'
+import updateLocale from '../../src/plugin/updateLocale'
 import utc from '../../src/plugin/utc'
 import '../../src/locale/ru'
 
@@ -142,4 +143,31 @@ it('Locale without relativeTime config fallback', () => {
   expect(dayjs().locale({
     name: 'test-locale'
   }).fromNow()).toEqual(expect.any(String))
+})
+
+it('Past and Future keys should support function for additional processing', () => {
+  dayjs.extend(updateLocale)
+  dayjs.updateLocale('en', {
+    relativeTime: {
+      future: input => `${input} modified`,
+      past: input => `${input} modified`,
+      s: 'just now',
+      m: ' 1 min',
+      mm: '%d min',
+      h: '1 hr',
+      hh: '%d hrs',
+      d: 'a day',
+      dd: '%d days',
+      M: 'a month',
+      MM: '%d months',
+      y: 'a year',
+      yy: '%d years'
+    }
+  })
+
+
+  const past = Date.now() - 1000
+  expect(dayjs(past).fromNow()).toEqual(' 1 min modified')
+  const future = Date.now() + 1000
+  expect(dayjs(future).fromNow()).toEqual(' 1 min modified')
 })
