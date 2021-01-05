4cc2b17783dddc66a198bcb06dd5d89b20baaf0b
iamkun <kunhello@outlook.com>
2021-01-06 07:38:03 +0800

    fix: update devHelper add warning "passing Year as a Number will be parsed as a Unix timestamp"

--- src/plugin/devHelper/index.js
+++ src/plugin/devHelper/index.js
@@ -8,6 +8,9 @@ export default (o, c, d) => {
       if (typeof date === 'string' && date.length === 13) {
         console.warn(`To parse a Unix timestamp like ${date}, you should pass it as a Number. https://day.js.org/docs/en/parse/unix-timestamp-milliseconds`)
       }
+      if (typeof date === 'number' && String(date).length === 4) {
+        console.warn(`Guessing you may want to parse the Year ${date}, you should pass it as a String ${date}, not a Number. Otherwise, ${date} will be treated as a Unix timestamp`)
+      }
       if (cfg.args.length >= 2 && !d.p.customParseFormat) {
         console.warn(`To parse a date-time string like ${date} using the given format, you should enable customParseFormat plugin first. https://day.js.org/docs/en/parse/string-format`)
       }
