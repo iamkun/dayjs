cc12aa5699455577cf41690b112645431c6ba623
Jason Kwok <JasonHK@users.noreply.github.com>
2021-03-08 01:19:26 +0800

    fix: fixed missing types for ArraySupport plugin

--- types/plugin/arraySupport.d.ts
+++ types/plugin/arraySupport.d.ts
@@ -1,4 +1,10 @@
 import { PluginFunc } from 'dayjs'
 
+declare module 'dayjs' {
+  interface ConfigTypeMap {
+    arraySupport: [year: number, month?: number, day?: number, hour?: number, minute?: number, second?: number, millisecond?: number]
+  }
+}
+
 declare const plugin: PluginFunc
 export = plugin
