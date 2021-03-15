9c20e77caf7b1b5eccf418175203b198d4e29535
Jamil Chioino <39961109+jamilchioino@users.noreply.github.com>
2021-03-14 22:50:33 -0500

    fix: update Spanish [es] locale to change month names to lowercase (#1414)

--- src/locale/es-us.js
+++ src/locale/es-us.js
@@ -6,7 +6,7 @@ const locale = {
   weekdays: 'domingo_lunes_martes_miércoles_jueves_viernes_sábado'.split('_'),
   weekdaysShort: 'dom._lun._mar._mié._jue._vie._sáb.'.split('_'),
   weekdaysMin: 'do_lu_ma_mi_ju_vi_sá'.split('_'),
-  months: 'Enero_Febrero_Marzo_Abril_Mayo_Junio_Julio_Agosto_Septiembre_Octubre_Noviembre_Diciembre'.split('_'),
+  months: 'enero_febrero_marzo_abril_mayo_junio_julio_agosto_septiembre_octubre_noviembre_diciembre'.split('_'),
   monthsShort: 'ene_feb_mar_abr_may_jun_jul_ago_sep_oct_nov_dic'.split('_'),
   relativeTime: {
     future: 'en %s',
