b51cb491652e03e42cd9b18c8f98a38dd297fc93
Annamalai <annamalai.saravanan@catapultsports.com>
2021-05-03 11:14:28 +1000

    fix: move en ordinal function to locale file

--- src/locale/en.js
+++ src/locale/en.js
@@ -3,5 +3,10 @@
 export default {
   name: 'en',
   weekdays: 'Sunday_Monday_Tuesday_Wednesday_Thursday_Friday_Saturday'.split('_'),
-  months: 'January_February_March_April_May_June_July_August_September_October_November_December'.split('_')
+  months: 'January_February_March_April_May_June_July_August_September_October_November_December'.split('_'),
+  ordinal: (number) => {
+    const s = ['th', 'st', 'nd', 'rd']
+    const v = number % 100
+    return `[${number}${(s[(v - 20) % 10] || s[v] || s[0])}]`
+  }
 }
