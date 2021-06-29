954f53a9e0943914d9414c62d3089882c5b710b4
Marcelo Shima <marceloshima@gmail.com>
2021-06-28 22:34:08 -0300

    Add esm locale test.

--- type-test/esModuleInterop.ts
+++ type-test/esModuleInterop.ts
@@ -4,7 +4,12 @@ import customParseFormat from 'dayjs/esm/plugin/customParseFormat';
 import duration from 'dayjs/esm/plugin/duration';
 import relativeTime from 'dayjs/esm/plugin/relativeTime';
 
+import 'dayjs/locale/es'
+
 new dayjs.Dayjs();
+
+dayjs.locale('es');
+dayjs('2018-05-05').locale('es').format()
 
 dayjs.extend(customParseFormat);
 dayjs.extend(duration);
