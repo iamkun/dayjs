2a849b6a657b60624d3bbc312142dd162b792e99
leoossa <leonard@leonardossa.com>
2021-08-07 04:37:43 +0200

    feat: add lojban support (jbo.js)

---
+++ src/locale/jbo.js
@@ -0,0 +1,6 @@
+// Lojban [jbo]
+export default {
+  name: 'jbo',
+  weekdays: 'nodjed._padjed._redjed._cidjed._vodjed._mudjed._xadjed.'.split('_'),
+  months: 'pamast._remast._cimast._vomast._mumast._xamast._zemast._bimast._somast._daumast._feimast._gaimast.'.split('_')
+}
