490329486faca3f6224fdb6a21acc1b52da32936
Eduards Jubels <eduards.jubels@ampedsoftware.com>
2022-01-07 11:19:03 +0100

    test: Add test to complete coverage for monthsOneIndexed

--- test/plugin/monthsOneIndexed.test.js
+++ test/plugin/monthsOneIndexed.test.js
@@ -13,7 +13,12 @@ afterEach(() => {
 })
 
 it('is correct month', () => {
-  for (let i = 1; i <= 12; i++) {
+  for (let i = 1; i <= 12; i += 1) {
     expect(dayjs().month(i).format('M')).toBe(i.toString())
   }
+})
+
+it('returns correct month', () => {
+  expect(dayjs().month()).toBeGreaterThanOrEqual(1)
+  expect(dayjs().month()).toBeLessThanOrEqual(12)
 })
