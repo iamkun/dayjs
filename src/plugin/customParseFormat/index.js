2ceea063deb030b89dee8831d553113d17bc7e06
Bernhard Pottler <bpottler@nexgo.de>
2022-05-27 06:57:57 +0200

    fix: avoid potential problems with dash after '\d'
    Dash could be misinterpreted as range operator.

--- src/plugin/customParseFormat/index.js
+++ src/plugin/customParseFormat/index.js
@@ -9,7 +9,7 @@ const match4 = /\d{4}/ // 0000 - 9999
 const match1to2 = /\d\d?/ // 0 - 99
 const matchSigned = /[+-]?\d+/ // -inf - inf
 const matchOffset = /[+-]\d\d:?(\d\d)?|Z/ // +00:00 -00:00 +0000 or -0000 +00 or Z
-const matchWord = /\d*[^\s\d-_:/,()]+/ // Word
+const matchWord = /\d*[^-_:/,()\s\d]+/ // Word
 
 let locale = {}
 
