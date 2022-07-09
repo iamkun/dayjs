323c2e10a8e8b1f263857cea95621da08f3b0b0c
Bernhard Pottler <bpottler@nexgo.de>
2022-07-09 19:54:32 +0200

    refactor: unify layout of plugin code

--- src/plugin/toArray.ts
+++ src/plugin/toArray.ts
@@ -1,24 +1,36 @@
 import type { Dayjs, Plugin } from '..'
 
+export type toArrayType = () => [
+  number,
+  number,
+  number,
+  number,
+  number,
+  number,
+  number
+]
+
 declare module '../types' {
   export interface Extend {
-    toArray: typeof toArray
+    toArray: toArrayType
   }
 }
 
-function toArray(
-  this: Dayjs
-): [number, number, number, number, number, number, number] {
-  return [
-    this.year(),
-    this.month(),
-    this.date(),
-    this.hour(),
-    this.minute(),
-    this.second(),
-    this.millisecond(),
-  ]
-}
+const plugin: Plugin = (cls) => {
+  const toArray: toArrayType = function (
+    this: Dayjs
+  ): [number, number, number, number, number, number, number] {
+    return [
+      this.year(),
+      this.month(),
+      this.date(),
+      this.hour(),
+      this.minute(),
+      this.second(),
+      this.millisecond(),
+    ]
+  }
 
-const plugin: Plugin = (cls) => (cls.prototype.toArray = toArray)
+  cls.prototype.toArray = toArray
+}
 export default plugin
