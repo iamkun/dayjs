ddd7a26d772fd446819cc3c37c49f2ade7586f8c
Bernhard Pottler <bpottler@nexgo.de>
2022-07-13 06:39:51 +0200

    refactor: fix casing of type name

--- src/plugin/isYesterday.ts
+++ src/plugin/isYesterday.ts
@@ -1,15 +1,15 @@
 import type { Dayjs, Plugin } from '..'
 
-export type isYesterdayType = () => boolean
+export type IsYesterday = () => boolean
 
 declare module '../types' {
   export interface Extend {
-    isYesterday: isYesterdayType
+    isYesterday: IsYesterday
   }
 }
 
 const plugin: Plugin = (cls, fn) => {
-  const isYesterday: isYesterdayType = function (this: Dayjs): boolean {
+  const isYesterday: IsYesterday = function (this: Dayjs): boolean {
     const comparisonTemplate = 'YYYY-MM-DD'
     const now = fn()
     return this.format(comparisonTemplate) === now.format(comparisonTemplate)
