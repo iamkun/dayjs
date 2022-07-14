d44ab1066aabeb6313e3aed3723d47ca9612861e
Bernhard Pottler <bpottler@nexgo.de>
2022-07-14 06:36:25 +0200

    refactor: return to original code layout after review

--- src/plugin/toArray.ts
+++ src/plugin/toArray.ts
@@ -1,36 +1,26 @@
 import type { Dayjs, Plugin } from '..'
 
-export type toArrayType = () => [
-  number,
-  number,
-  number,
-  number,
-  number,
-  number,
-  number
-]
-
 declare module '../types' {
   export interface Extend {
-    toArray: toArrayType
+    toArray: typeof toArray
   }
 }
 
-const plugin: Plugin = (cls) => {
-  const toArray: toArrayType = function (
-    this: Dayjs
-  ): [number, number, number, number, number, number, number] {
-    return [
-      this.year(),
-      this.month(),
-      this.date(),
-      this.hour(),
-      this.minute(),
-      this.second(),
-      this.millisecond(),
-    ]
-  }
+function toArray(
+  this: Dayjs
+): [number, number, number, number, number, number, number] {
+  return [
+    this.year(),
+    this.month(),
+    this.date(),
+    this.hour(),
+    this.minute(),
+    this.second(),
+    this.millisecond(),
+  ]
+}
 
+const plugin: Plugin = (cls) => {
   cls.prototype.toArray = toArray
 }
 export default plugin
