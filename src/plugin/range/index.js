c569241a693f87bb446ae7f452b373d421eb3b0a
Olly Harkness <oliver.harkness@boject.com>
2022-07-22 15:58:00 +0100

    feature/range

---
+++ src/plugin/range/index.js
@@ -0,0 +1,52 @@
+export class DayjsRange {
+  constructor(startDate, endDate) {
+    const { s, e } = startDate > endDate
+      ? { s: endDate, e: startDate }
+      : { s: startDate, e: endDate }
+
+    this.startDate = s
+    this.endDate = e
+  }
+
+  isValidRange() {
+    return this.startDate.isValid() && this.endDate.isValid()
+  }
+
+  clone() {
+    return new DayjsRange(this.startDate, this.endDate)
+  }
+
+  isOverlap(other) {
+    if (!(other instanceof DayjsRange) || !(other.isValidRange())) {
+      return false
+    }
+
+    return this.startDate < other.endDate && this.endDate > other.startDate
+  }
+
+  isEqual(other) {
+    return this.startDate.isSame(other.startDate) && this.endDate.isSame(other.endDate)
+  }
+
+  addStartRange(number, unit) {
+    return new DayjsRange(this.startDate.add(number, unit), this.endDate)
+  }
+
+  addEndRange(number, unit) {
+    return new DayjsRange(this.startDate, this.endDate.add(number, unit))
+  }
+
+  subtractStartRange(number, unit) {
+    return new DayjsRange(this.startDate.subtract(number, unit), this.endDate)
+  }
+
+  subtractEndRange(number, unit) {
+    return new DayjsRange(this.startDate, this.endDate.subtract(number, unit))
+  }
+}
+
+export default(o, c, d) => {
+  d.range = function (start, end) {
+    return new DayjsRange(d(start), d(end))
+  }
+}
