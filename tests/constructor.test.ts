7d55a4f0384ef7365ff2a2450c817c9fb065e936
Bernhard Pottler <bpottler@nexgo.de>
2022-08-09 17:59:31 +0200

    test: extend constructor tests

--- tests/constructor.test.ts
+++ tests/constructor.test.ts
@@ -1,14 +1,81 @@
-import { expect, it } from 'vitest'
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 import { Dayjs, dayjs, unix } from '../src'
 
-it('supports instanceof dayjs', () => {
-  expect(dayjs() instanceof Dayjs).toBeTruthy()
-})
+describe('dayjs constructor', () => {
+  beforeEach(() => {
+    vi.setSystemTime(new Date())
+  })
 
-it('does not break isDayjs', () => {
-  expect(dayjs.isDayjs(dayjs())).toBeTruthy()
-})
+  afterEach(() => {
+    vi.useRealTimers()
+  })
 
-it('should parse from unix timestamp', () => {
-  expect(unix(86400).valueOf()).toBe(86400000)
+  it('creates instance of Dayjs', () => {
+    expect(dayjs() instanceof Dayjs).toBeTruthy()
+  })
+
+  it('does not break isDayjs', () => {
+    expect(dayjs.isDayjs(dayjs())).toBeTruthy()
+  })
+
+  it('creates valid date from no input parameter', () => {
+    const newDate = dayjs()
+    expect(newDate instanceof Dayjs).toBeTruthy()
+    expect(newDate.isValid()).toBeTruthy()
+  })
+
+  it('creates valid date from string', () => {
+    const newDate = dayjs('2022-04-05T16:25:36.987Z')
+    expect(newDate instanceof Dayjs).toBeTruthy()
+    expect(newDate.isValid()).toBeTruthy()
+  })
+
+  it('creates valid date from number', () => {
+    const newDate = dayjs(86400)
+    expect(newDate instanceof Dayjs).toBeTruthy()
+    expect(newDate.isValid()).toBeTruthy()
+    expect(newDate.valueOf()).toBe(86400)
+  })
+
+  it('should parse from unix timestamp', () => {
+    const newDate = unix(86400)
+    expect(newDate instanceof Dayjs).toBeTruthy()
+    expect(newDate.isValid()).toBeTruthy()
+    expect(newDate.valueOf()).toBe(86400000)
+  })
+
+  it('creates valid date from empty object', () => {
+    const newDate = dayjs({})
+    expect(newDate instanceof Dayjs).toBeTruthy()
+    expect(newDate.isValid()).toBeTruthy()
+  })
+
+  it('creates valid date from Dayjs object', () => {
+    const basicDateObject = dayjs()
+    expect(basicDateObject instanceof Dayjs).toBeTruthy()
+    const newDate = dayjs(basicDateObject)
+    expect(newDate !== undefined).toBeTruthy()
+    expect(newDate instanceof Dayjs).toBeTruthy()
+    expect(newDate.isValid()).toBeTruthy()
+  })
+
+  it('creates valid date from Date object', () => {
+    const basicDateObject = new Date()
+    expect(basicDateObject instanceof Date).toBeTruthy()
+    const newDate = dayjs(basicDateObject)
+    expect(newDate instanceof Dayjs).toBeTruthy()
+    expect(newDate.isValid()).toBeTruthy()
+  })
+
+  it('creates valid date from null', () => {
+    const newDate = dayjs(null)
+    expect(newDate instanceof Dayjs).toBeTruthy()
+    expect(newDate.isValid()).toBeFalsy()
+  })
+
+  it('creates valid date from undefined', () => {
+    const newDate = dayjs(undefined)
+    expect(newDate instanceof Dayjs).toBeTruthy()
+    expect(newDate.isValid()).toBeTruthy()
+  })
 })
