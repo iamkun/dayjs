a3c3399ff43b4d6548c10c9c91d54148d4de03e2
Bernhard Pottler <bpottler@nexgo.de>
2022-09-04 07:14:31 +0200

    feat: add locale 'en-gb' that is required by the tests

---
+++ src/locale/en-gb.ts
@@ -0,0 +1,78 @@
+// English (United Kingdom) [en-gb]
+
+import type { Locale } from '.'
+
+const locale: Locale = {
+  name: 'en-gb',
+  weekdays: [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ],
+  weekdaysShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+  weekdaysMin: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+  months: [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ],
+  monthsShort: [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ],
+  weekStart: 1,
+  yearStart: 4,
+  relativeTime: {
+    future: 'in %s',
+    past: '%s ago',
+    s: 'a few seconds',
+    m: 'a minute',
+    mm: '%d minutes',
+    h: 'an hour',
+    hh: '%d hours',
+    d: 'a day',
+    dd: '%d days',
+    M: 'a month',
+    MM: '%d months',
+    y: 'a year',
+    yy: '%d years',
+  },
+  formats: {
+    LT: 'HH:mm',
+    LTS: 'HH:mm:ss',
+    L: 'DD/MM/YYYY',
+    LL: 'D MMMM YYYY',
+    LLL: 'D MMMM YYYY HH:mm',
+    LLLL: 'dddd, D MMMM YYYY HH:mm',
+  },
+  ordinal: (n) => {
+    const s = ['th', 'st', 'nd', 'rd']
+    const v = +n % 100
+    return `[${n}${s[(v - 20) % 10] || s[v] || s[0]}]`
+  },
+}
+
+export default locale
