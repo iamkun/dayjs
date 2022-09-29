30f0d3cb9ccb860a2105d35afd40bd01b07a6a27
ajisulist <ajisulist@gmail.com>
2022-09-29 09:05:59 +0700

    [fix] partial locale update

--- src/plugin/updateLocale/index.js
+++ src/plugin/updateLocale/index.js
@@ -1,3 +1,5 @@
+import { extend } from './utils'
+
 export default (option, Dayjs, dayjs) => {
   dayjs.updateLocale = function (locale, customConfig) {
     const localeList = dayjs.Ls
@@ -5,9 +7,8 @@ export default (option, Dayjs, dayjs) => {
     if (!localeConfig) return
     const customConfigKeys = customConfig ? Object.keys(customConfig) : []
     customConfigKeys.forEach((c) => {
-      localeConfig[c] = customConfig[c]
+      localeConfig[c] = typeof localeConfig[c] === 'object' ? extend(localeConfig[c], customConfig[c]) : customConfig[c]
     })
     return localeConfig // eslint-disable-line consistent-return
   }
 }
-
