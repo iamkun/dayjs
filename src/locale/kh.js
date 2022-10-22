c5ef377d3944c4f065a41a22876a8de25c87a83c
loma-cambodia <109500535+loma-cambodia@users.noreply.github.com>
2022-10-22 11:29:05 +0700

    kh locale

---
+++ src/locale/kh.js
@@ -0,0 +1,63 @@
+import dayjs from 'dayjs'
+
+const locale = {
+  name: 'en-nz',
+  weekdays: 'Sunday_Monday_Tuesday_Wednesday_Thursday_Friday_Saturday'.split(
+    '_'
+  ),
+  months:
+    'January_February_March_April_May_June_July_August_September_October_November_December'.split(
+      '_'
+    ),
+  weekStart: 1,
+  weekdaysShort: 'Sun_Mon_Tue_Wed_Thu_Fri_Sat'.split('_'),
+  monthsShort: 'Jan_Feb_Mar_Apr_May_Jun_Jul_Aug_Sep_Oct_Nov_Dec'.split('_'),
+  weekdaysMin: 'Su_Mo_Tu_We_Th_Fr_Sa'.split('_'),
+  ordinal: (n) => {
+    const s = ['th', 'st', 'nd', 'rd']
+    const v = n % 100
+    return `[${n}${s[(v - 20) % 10] || s[v] || s[0]}]`
+  },
+  formats: {
+    LT: 'h:mm A',
+    LTS: 'h:mm:ss A',
+    L: 'DD/MM/YYYY',
+    LL: 'D MMMM YYYY',
+    LLL: 'D MMMM YYYY h:mm A',
+    LLLL: 'dddd, D MMMM YYYY h:mm A',
+  },
+  relativeTime: {
+    future: 'in %s',
+    past: '%s ago',
+    s: 'a few seconds',
+    m: 'a minute',
+    mm: '%d minutes',
+    h: 'an hour',
+    hh: '%d hours',
+    d: 'a day',
+    dd: '%d days',
+    M: 'a month',
+    MM: '%d months',
+    y: 'a year',
+    yy: '%d years',
+  },
+  meridiem: (hour, minute) => {
+    const hm = hour * 100 + minute
+    if (hm < 600) {
+      return '凌晨'
+    } else if (hm < 900) {
+      return '早上'
+    } else if (hm < 1100) {
+      return '上午'
+    } else if (hm < 1300) {
+      return '中午'
+    } else if (hm < 1800) {
+      return '下午'
+    }
+    return '晚上'
+  },
+}
+
+dayjs.locale(locale, null, true)
+
+export default locale
