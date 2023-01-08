a20a11262afe232be781370b2c8d80f9b682e04e
Trim21 <trim21.me@gmail.com>
2023-01-08 20:37:16 +0800

    promisify on import

--- build/esm.js
+++ build/esm.js
@@ -1,9 +1,10 @@
-const fsp = require('fs/promises')
+const fs = require('fs/promises')
 const path = require('path')
 const util = require('util')
-const { ncp } = require('ncp')
 
 const { promisify } = util
+
+const ncp = promisify(require('ncp'))
 
 const typeFileExt = '.d.ts'
 const localeDir = path.join(process.env.PWD, 'esm/locale')
@@ -12,12 +13,12 @@ const localeTypePath = path.join(process.env.PWD, 'esm/locale', `index${typeFile
 
 
 async function walk(dir, callback) {
-  const files = await fsp.readdir(dir)
+  const files = await fs.readdir(dir)
 
   /* eslint-disable no-restricted-syntax, no-await-in-loop */
   for (const file of files) {
     const filepath = path.join(dir, file)
-    const stats = await fsp.stat(filepath)
+    const stats = await fs.stat(filepath)
     if (stats.isDirectory()) {
       await walk(filepath, callback)
     } else if (stats.isFile()) {
@@ -29,45 +30,45 @@ async function walk(dir, callback) {
 
 (async () => {
   try {
-    const readLocaleDir = await fsp.readdir(localeDir)
+    const readLocaleDir = await fs.readdir(localeDir)
     await Promise.all(readLocaleDir.map(l => async () => {
       const filePath = path.join(localeDir, l)
-      const readFile = await fsp.readFile(filePath, 'utf8')
+      const readFile = await fs.readFile(filePath, 'utf8')
       const result = readFile.replace("'dayjs'", "'../index'")
-      await fsp.writeFile(filePath, result, 'utf8')
+      await fs.writeFile(filePath, result, 'utf8')
     })
       .map(f => f()))
 
-    await promisify(ncp)('./types/', './esm')
+    await ncp('./types/', './esm')
 
-    const readLocaleFile = await fsp.readFile(localeTypePath, 'utf8')
+    const readLocaleFile = await fs.readFile(localeTypePath, 'utf8')
     const localResult = readLocaleFile.replace("'dayjs", "'dayjs/esm")
-    await fsp.writeFile(localeTypePath, localResult, 'utf8')
+    await fs.writeFile(localeTypePath, localResult, 'utf8')
 
-    const readPluginDir = await fsp.readdir(pluginDir)
+    const readPluginDir = await fs.readdir(pluginDir)
 
     await Promise.all(readPluginDir.map(p => async () => {
       if (p.includes(typeFileExt)) {
         const pluginName = p.replace(typeFileExt, '')
         const filePath = path.join(pluginDir, p)
         const targetPath = path.join(pluginDir, pluginName, `index${typeFileExt}`)
-        const readFile = await fsp.readFile(filePath, 'utf8')
+        const readFile = await fs.readFile(filePath, 'utf8')
         const result = readFile.replace(/'dayjs'/g, '\'dayjs/esm\'')
-        await fsp.writeFile(targetPath, result, 'utf8')
-        await fsp.unlink(filePath)
+        await fs.writeFile(targetPath, result, 'utf8')
+        await fs.unlink(filePath)
       }
     })
       .map(f => f()))
 
     await walk('./esm/', async (p) => {
       if (p.endsWith('.js')) {
-        await fsp.rename(p, p.replace(/\.js$/g, '.mjs'))
+        await fs.rename(p, p.replace(/\.js$/g, '.mjs'))
       } else if (p.endsWith(typeFileExt)) {
-        const content = await fsp.readFile(p, 'utf8')
-        await fsp.writeFile(p, content.replaceAll('export = ', 'export default '))
+        const content = await fs.readFile(p, 'utf8')
+        await fs.writeFile(p, content.replaceAll('export = ', 'export default '))
       }
     })
-    await promisify(ncp)(path.join(__dirname, 'esm-wrapper/'), './esm')
+    await ncp(path.join(__dirname, 'esm-wrapper/'), './esm')
   } catch (e) {
     console.error(e) // eslint-disable-line no-console
   }
