3e85187e1640a263ba4beed76b34b5ace27a045a
Trim21 <trim21.me@gmail.com>
2023-01-08 20:12:16 +0800

    add comments

--- build/esm-wrapper/index.js
+++ build/esm-wrapper/index.js
@@ -1,3 +1,11 @@
+/**
+ * You should not import this file,
+ *
+ * this file exists only to keep backward compatibility for bundlers.
+ *
+ * `import dayjs from "dayjs/esm/index.mjs"` instead.
+ */
+
 // eslint-disable-next-line import/no-unresolved
 import dayjs from './index.mjs'
 
