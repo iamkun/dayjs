fd7aa1484bf76ed64792aaf646ebb3074780ce34
Сеничев Александр Вадимович <senichev@tochka.com>
2023-03-03 16:39:31 +0300

    fix: Invalid month number with customParseFormat leads to the next year date

--- test/plugin/customParseFormat.test.js
+++ test/plugin/customParseFormat.test.js
@@ -281,6 +281,16 @@ it('correctly parse ordinal', () => {
     .toBe(momentCN.locale())
 })
 
+describe('month doesn\'t lead to the next year', () => {
+  it('MMMM', () => {
+    const input = '03 декабря'
+    const format1 = 'D MMMM'
+    const format2 = 'D MMMMF'
+    expect(dayjs(input, format1, 'ru').valueOf()).toBe(moment(input, format1, 'ru').valueOf())
+    expect(dayjs(input, format2, 'ru').valueOf()).toBe(moment(input, format2, 'ru').valueOf())
+  })
+})
+
 describe('month function locale', () => {
   it('MMMM', () => {
     const input = '08 мая 2020'
