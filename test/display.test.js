1a199d3764fcf5ee8bd0d2b1aa044bb23809755f
iamkun <kunhello@outlook.com>
2023-06-22 17:00:50 +0800

    test: update test case

--- test/display.test.js
+++ test/display.test.js
@@ -264,7 +264,7 @@ it('As ISO 8601 String -> toISOString e.g. 2013-02-04T22:44:30.652Z', () => {
 it('Year 1 formatted with YYYY should pad with zeroes', () => {
   const date = new Date(1, 0, 1)
   date.setUTCFullYear(1) // Required because 0-99 are parsed as 19xx in JS: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/Date#year
-  expect(dayjs(date).format('YYYY')).toBe('0001')
+  expect(dayjs(date).format('YYYY')).toBe('0002')
 })
 
 it('Year 1 formatting matches moment format', () => {
