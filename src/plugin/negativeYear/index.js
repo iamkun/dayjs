982c197f0238541687b18fb72d94e1e4ed8bee93
klm-turing <litaaba.a@turing.com>
2024-05-02 18:10:20 +0000

    Make sure that negativeYear plugin worked in harmony with other plugins

--- src/plugin/negativeYear/index.js
+++ src/plugin/negativeYear/index.js
@@ -3,10 +3,13 @@ export default (_, c, dayjs) => {
 
   const parseDate = (cfg) => {
     const { date } = cfg
-    const newDate = new Date(date)
-    const fullYear = newDate.getFullYear()
-    if (typeof date === 'string' && date.indexOf(`-${fullYear}`) !== -1) {
-      return dayjs(newDate).subtract(fullYear * 2, 'year').toDate()
+    if (typeof date === 'string' && date.charAt(0) === '-') {
+      const newDate = new Date(date.slice(1))
+      const fullYear = newDate.getFullYear()
+      if (date.indexOf(`-${fullYear}`) !== -1) {
+        return dayjs(newDate).subtract(fullYear * 2, 'year').toDate()
+      }
+      return date
     }
     return date
   }
