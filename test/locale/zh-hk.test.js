83d9e0c5d7f3be2d829daa17ef5b91028ada5ae1
iamkun <kunhello@outlook.com>
2025-08-27 17:38:44 +0800

    test: update test case (#2917)

--- test/locale/zh-hk.test.js
+++ test/locale/zh-hk.test.js
@@ -27,6 +27,8 @@ test('Meridiem', () => {
 })
 
 test('Meridiem', () => {
+  expect(dayjs('2020-01-01 05:59:59').locale('zh-hk').format('A')).toEqual('凌晨')
+  expect(dayjs('2020-01-01 08:59:59').locale('zh-hk').format('A')).toEqual('早上')
   expect(dayjs('2020-01-01 10:59:59').locale('zh-hk').format('A')).toEqual('上午')
   expect(dayjs('2020-01-01 11:00:00').locale('zh-hk').format('A')).toEqual('中午')
   expect(dayjs('2020-01-01 12:59:59').locale('zh-hk').format('A')).toEqual('中午')
