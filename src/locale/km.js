bbe4ab1bdb1042667817de2433fc85e6b12eadad
iamkun <kunhello@outlook.com>
2026-03-12 19:19:35 +0800

    chore: fix lint error

--- src/locale/km.js
+++ src/locale/km.js
@@ -33,7 +33,7 @@ const locale = {
     y: 'មួយឆ្នាំ',
     yy: '%d ឆ្នាំ'
   },
-  meridiem: (hour) => (hour > 12 ? 'ល្ងាច' : 'ព្រឹក')
+  meridiem: hour => (hour > 12 ? 'ល្ងាច' : 'ព្រឹក')
 }
 
 dayjs.locale(locale, null, true)
