84e362d2413cca8082f2ff2375ff38c7ac049061
Rahul Pratik <rahul.pratik@aexp.com>
2026-05-24 13:01:43 -0700

    fix: Update Swahili locale(sw) yearStart 4

--- src/locale/sw.js
+++ src/locale/sw.js
@@ -9,6 +9,7 @@ const locale = {
   months: 'Januari_Februari_Machi_Aprili_Mei_Juni_Julai_Agosti_Septemba_Oktoba_Novemba_Desemba'.split('_'),
   monthsShort: 'Jan_Feb_Mac_Apr_Mei_Jun_Jul_Ago_Sep_Okt_Nov_Des'.split('_'),
   weekStart: 1,
+  yearStart: 4,
   ordinal: n => n,
   relativeTime: {
     future: '%s baadaye',
