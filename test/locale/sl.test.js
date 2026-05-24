a324f72dc389521f01b9a6bbcf5e4a95d2c68bfb
Rahul Pratik <rahul.pratik@aexp.com>
2026-05-24 12:33:13 -0700

    fix: Update Slovenian locale(sl) yearStart 4

--- test/locale/sl.test.js
+++ test/locale/sl.test.js
@@ -1,9 +1,12 @@
+import moment from 'moment'
 import MockDate from 'mockdate'
 import dayjs from '../../src'
+import weekOfYear from '../../src/plugin/weekOfYear'
 import relativeTime from '../../src/plugin/relativeTime'
 import '../../src/locale/sl'
 
 dayjs.extend(relativeTime)
+dayjs.extend(weekOfYear)
 
 beforeEach(() => {
   MockDate.set(new Date())
@@ -78,5 +81,34 @@ it('Slovenian locale relative time in past and future', () => {
     expect(dayjs().add(c[0], c[1]).locale('sl').fromNow()).toBe(c[2])
     // Without suffix
     expect(dayjs().add(c[0], c[1]).locale('sl').fromNow(true)).toBe(c[3])
+  })
+})
+describe('yearStart: 4 - ISO 8601 week numbering (sl locale)', () => {
+  // yearStart: 4 means the first week of the year is the one containing Jan 4
+  // (ISO 8601: week 1 is the week with the year's first Thursday)
+  const cases = [
+    // date          expected week  description
+    ['2018-12-31', 1, 'Dec 31 2018 belongs to week 1 of 2019'],
+    ['2019-01-01', 1, 'Jan 1 2019 is week 1'],
+    ['2019-01-06', 1, 'Jan 6 2019 (Sunday) is still week 1'],
+    ['2019-01-07', 2, 'Jan 7 2019 (Monday) starts week 2'],
+    ['2019-12-30', 1, 'Dec 30 2019 rolls into week 1 of 2020'],
+    ['2020-01-01', 1, 'Jan 1 2020 is week 1']
+  ]
+
+  cases.forEach(([date, expectedWeek, description]) => {
+    it(description, () => {
+      dayjs.locale('sl')
+      moment.locale('sl')
+      expect(dayjs(date).week()).toBe(expectedWeek)
+      expect(dayjs(date).week()).toBe(moment(date).week())
+    })
+  })
+
+  it('locale object exposes yearStart as 4', () => {
+    const slLocale = dayjs()
+      .locale('sl')
+      .$locale()
+    expect(slLocale.yearStart).toBe(4)
   })
 })
