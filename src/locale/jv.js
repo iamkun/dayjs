69f9b530cb9397d5310af1b236d6fa61618a3e80
Rahul Pratik <rahul.pratik@aexp.com>
2026-05-24 11:50:17 -0700

    fix: Update Javanese locale(jv) yearStart 4

--- src/locale/jv.js
+++ src/locale/jv.js
@@ -6,6 +6,7 @@ const locale = {
   weekdays: 'Minggu_Senen_Seloso_Rebu_Kemis_Jemuwah_Septu'.split('_'),
   months: 'Januari_Februari_Maret_April_Mei_Juni_Juli_Agustus_September_Oktober_Nopember_Desember'.split('_'),
   weekStart: 1,
+  yearStart: 4,
   weekdaysShort: 'Min_Sen_Sel_Reb_Kem_Jem_Sep'.split('_'),
   monthsShort: 'Jan_Feb_Mar_Apr_Mei_Jun_Jul_Ags_Sep_Okt_Nop_Des'.split('_'),
   weekdaysMin: 'Mg_Sn_Sl_Rb_Km_Jm_Sp'.split('_'),
