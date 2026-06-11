f05d7bbbccb002b4d7e0045ea4ed13306de6f7fd
Alex Trukhnii <john.tacker@gmail.com>
2026-06-11 12:07:20 +0200

    feat(locale): add meridiem to Ukrainian (uk) locale

--- src/locale/uk.js
+++ src/locale/uk.js
@@ -61,6 +61,16 @@ const locale = {
     yy: relativeTimeWithPlural
   },
   ordinal: n => n,
+  meridiem: (hour) => {
+    if (hour < 4) {
+      return 'ночі'
+    } else if (hour < 12) {
+      return 'ранку'
+    } else if (hour < 17) {
+      return 'дня'
+    }
+    return 'вечора'
+  },
   formats: {
     LT: 'HH:mm',
     LTS: 'HH:mm:ss',
