f85bfd941a69c812cb3c765e6508de7d08501cd1
greymoth <246701683+greymoth-jp@users.noreply.github.com>
2026-06-30 03:46:24 +0900

    fix(locale): use correct English ordinal in en-ca, en-ie, en-il, en-sg
    These four English-variant locales kept the placeholder ordinal n => n, so
    format('Do') returned the bare day number ("21" instead of "21st"). The
    other English locales (en, en-au, en-gb, en-in, en-nz, en-tt) already use
    the standard English ordinal function; reuse it here so all English
    variants agree. Matches moment's ordinal for these locales (see #2878).

--- test/locale/en.test.js
+++ test/locale/en.test.js
@@ -1,11 +1,18 @@
+import moment from 'moment'
 import dayjs from '../../src'
 import '../../src/locale/en'
 import '../../src/locale/en-gb'
 import '../../src/locale/en-in'
 import '../../src/locale/en-tt'
+import '../../src/locale/en-ca'
+import '../../src/locale/en-ie'
+import '../../src/locale/en-il'
+import '../../src/locale/en-sg'
 import localizedFormat from '../../src/plugin/localizedFormat'
+import advancedFormat from '../../src/plugin/advancedFormat'
 
 dayjs.extend(localizedFormat)
+dayjs.extend(advancedFormat)
 
 const locales = [
   { locale: 'en', expectedDate: '12/25/2019' },
@@ -19,6 +26,19 @@ describe('English date formats', () => {
     it(`should correctly format date with locale - ${locale.locale}`, () => {
       const dayjsWithLocale = dayjs('2019-12-25').locale(locale.locale)
       expect(dayjsWithLocale.format('L')).toEqual(locale.expectedDate)
+    })
+  })
+})
+
+describe('English ordinal (Do) parity with moment', () => {
+  const ordinalLocales = ['en-ca', 'en-ie', 'en-il', 'en-sg']
+  ordinalLocales.forEach((locale) => {
+    it(`should format the ordinal day of month like moment - ${locale}`, () => {
+      for (let d = 1; d <= 31; d += 1) {
+        const date = `2021-01-${String(d).padStart(2, '0')}`
+        expect(dayjs(date).locale(locale).format('Do'))
+          .toEqual(moment(date).locale(locale).format('Do'))
+      }
     })
   })
 })
